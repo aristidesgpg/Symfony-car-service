c5f4a4d80def92cb3536b9c1689562b627713ff3
Laramie Rugen <ffgpga08@gmail.com>
2020-09-23 11:34:11 -0400

    removed twig/profiler. not using

--- src/Controller/AuthenticationController.php
+++ src/Controller/AuthenticationController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -19,11 +18,13 @@ class AuthenticationController {
     /**
      * @Route("/authenticate", methods={"POST"})
      *
-     * @param Request         $request
+     * @param Request          $request
      *
      * @return JsonResponse
      */
     public function authenticate (Request $request) {
-        return new JsonResponse(['test' => 'test']);
+        $test = ['test' => 'test'];
+
+        return new JsonResponse($test);
     }
 }
