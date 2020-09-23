b107b17d72eb684b00b2db97e65994a92d065817
Laramie Rugen <ffgpga08@gmail.com>
2020-09-23 09:49:07 -0400

    init

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class DefaultController
+ *
+ * @package App\Controller
+ */
+class DefaultController extends AbstractController {
+
+    /**
+     * @Route("/")
+     *
+     * @return Response
+     */
+    public function index () {
+        return new Response('home page');
+    }
+
+    /**
+     * @Route("/test/{slug}")
+     *
+     * @param $slug
+     *
+     * @return Response
+     */
+    public function test ($slug) {
+        return new Response($slug);
+    }
+}
