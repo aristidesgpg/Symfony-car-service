1907345d91b098775d68401c09d4a7fb090ecb52
Laramie Rugen <ffgpga08@gmail.com>
2020-09-23 09:55:53 -0400

    sec-checker bundle

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,7 +10,7 @@ use Symfony\Component\Routing\Annotation\Route;
  *
  * @package App\Controller
  */
-class DefaultController extends AbstractController {
+class DefaultController {
 
     /**
      * @Route("/")
