bb6a355ac4251517bab2360c3cb8a36f3d5f73f1
Tyler Holcomb <tyler@tholcomb.com>
2020-10-08 17:00:29 -0400

    Added CustomerController and CustomerHelper

---
+++ src/Controller/CustomerController.php
@@ -0,0 +1,191 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Customer;
+use App\Entity\User;
+use App\Repository\CustomerRepository;
+use App\Response\ValidationItem;
+use App\Service\CustomerHelper;
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use Swagger\Annotations as SWG;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+/**
+ * Class CustomerController
+ * @package App\Controller
+ * @Rest\Route("/api/customer")
+ * @SWG\Tag(name="Customer")
+ */
+class CustomerController extends AbstractFOSRestController {
+    /** @var CustomerRepository */
+    private $repo;
+
+    /**
+     * CustomerController constructor.
+     * @param CustomerRepository $repo
+     */
+    public function __construct (CustomerRepository $repo) {
+        $this->repo = $repo;
+    }
+
+    /**
+     * @Rest\Get
+     * @SWG\Response(
+     *     response="200",
+     *     description="Success!",
+     *     @SWG\Schema(
+     *         type="array",
+     *         @SWG\Items(ref=@Model(type=Customer::class, groups={"customer_list"}))
+     *     )
+     * )
+     *
+     * @return Response
+     */
+    public function getAll (): Response {
+        return $this->customerView($this->repo->findAllActive());
+    }
+
+    /**
+     * @Rest\Get("/{id}", name="getCustomer")
+     * @SWG\Response(
+     *     response="200",
+     *     description="Success!",
+     *     @SWG\Schema(type="object", ref=@Model(type=Customer::class, groups={"customer_list"}))
+     * )
+     * @SWG\Response(response="404", description="Customer does not exist")
+     *
+     * @param int $id
+     *
+     * @return Response
+     */
+    public function getCustomer (int $id): Response {
+        return $this->customerView($this->findCustomer($id));
+    }
+
+    /**
+     * @Rest\Post
+     * @SWG\Response(
+     *     response="201",
+     *     description="Success!",
+     *     headers={@SWG\Header(header="Location", type="string", description="URL of new customer")}
+     * )
+     * @SWG\Response(
+     *     response="406",
+     *     description="Validation failure",
+     *     @SWG\Schema(type="array", items=@SWG\Schema(type="object", ref=@Model(type=ValidationItem::class)))
+     * )
+     *
+     * @SWG\Parameter(name="firstName", type="string", in="formData", required=True)
+     * @SWG\Parameter(name="lastName", type="string", in="formData", required=True)
+     * @SWG\Parameter(name="phone", type="string", in="formData", required=True, minLength=10, maxLength=10)
+     * @SWG\Parameter(name="email", type="string", in="formData")
+     * @SWG\Parameter(name="doNotContact", type="boolean", in="formData")
+     *
+     * @param Request        $req
+     * @param CustomerHelper $helper
+     *
+     * @return Response
+     */
+    public function addCustomer (Request $req, CustomerHelper $helper): Response {
+        $validation = $helper->validateParams($req->request->all(), true);
+        if (!empty($validation)) {
+            return $this->handleView($this->view($validation, 406));
+        }
+        $customer = new Customer();
+        $customer->setAddedBy($this->getCurrentUser());
+        $helper->commitCustomer($customer, $req->request->all());
+        $url = $this->generateUrl('getCustomer', ['id' => $customer->getId()]);
+
+        return new Response(null, 201, ['Location' => $url]);
+    }
+
+    /**
+     * @Rest\Put("/{id}")
+     * @SWG\Response(response="200", description="Success!")
+     * @SWG\Response(response="404", description="Customer does not exist")
+     * @SWG\Response(
+     *     response="406",
+     *     description="Validation failure",
+     *     @SWG\Schema(type="array", items=@SWG\Schema(type="object", ref=@Model(type=ValidationItem::class)))
+     * )
+     *
+     * @SWG\Parameter(name="firstName", type="string", in="formData")
+     * @SWG\Parameter(name="lastName", type="string", in="formData")
+     * @SWG\Parameter(name="phone", type="string", in="formData")
+     * @SWG\Parameter(name="email", type="string", in="formData")
+     * @SWG\Parameter(name="doNotContact", type="boolean", in="formData")
+     *
+     * @param int            $id
+     * @param Request        $req
+     * @param CustomerHelper $helper
+     *
+     * @return Response
+     */
+    public function updateCustomer (int $id, Request $req, CustomerHelper $helper): Response {
+        $customer = $this->findCustomer($id);
+        $validation = $helper->validateParams($req->request->all());
+        if (!empty($validation)) {
+            return $this->handleView($this->view($validation, 406));
+        }
+        $helper->commitCustomer($customer, $req->request->all());
+
+        return new Response();
+    }
+
+    /**
+     * @Rest\Delete("/{id}")
+     * @SWG\Response(response="200", description="Success!")
+     * @SWG\Response(response="404", description="Customer does not exist")
+     *
+     * @param int            $id
+     * @param CustomerHelper $helper
+     *
+     * @return Response
+     */
+    public function deleteCustomer (int $id, CustomerHelper $helper): Response {
+        $customer = $this->findCustomer($id);
+        $customer->setDeleted(true);
+        $helper->commitCustomer($customer);
+
+        return new Response();
+    }
+
+    /**
+     * @param mixed $data
+     *
+     * @return Response
+     */
+    private function customerView ($data): Response {
+        $view = $this->view($data);
+        $view->getContext()->setGroups(['customer_list']);
+
+        return $this->handleView($view);
+    }
+
+    /**
+     * @param int $id
+     *
+     * @return Customer
+     * @throws NotFoundHttpException
+     */
+    private function findCustomer (int $id): Customer {
+        $customer = $this->repo->findActive($id);
+        if ($customer === null) {
+            throw new NotFoundHttpException(sprintf('Customer ID %d not found', $id));
+        }
+
+        return $customer;
+    }
+
+    /**
+     * @return User
+     */
+    private function getCurrentUser (): User {
+        return $this->container->get('security.token_storage')->getToken()->getUser();
+    }
+}
---
+++ src/Service/CustomerHelper.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Customer;
+use App\Entity\User;
+use App\Response\ValidationItem;
+use Doctrine\ORM\EntityManagerInterface;
+
+/**
+ * Class CustomerHelper
+ * @package App\Service
+ */
+class CustomerHelper {
+    private const REQUIRED_FIELDS = ['firstName', 'lastName', 'phone'];
+
+    private $em;
+    private $phoneValidator;
+
+    /**
+     * CustomerHelper constructor.
+     * @param EntityManagerInterface $em
+     * @param PhoneValidator         $phoneValidator
+     */
+    public function __construct(EntityManagerInterface $em, PhoneValidator $phoneValidator) {
+        $this->em = $em;
+        $this->phoneValidator = $phoneValidator;
+    }
+
+    /**
+     * @param array $params
+     * @param bool  $checkRequiredFields
+     *
+     * @return ValidationItem[] Empty on successful validation
+     */
+    public function validateParams(array $params, bool $checkRequiredFields = false): array {
+        $errors = [];
+        if ($checkRequiredFields === true) {
+            foreach (self::REQUIRED_FIELDS as $field) {
+                if (!isset($params[$field])) {
+                    $errors[$field] = 'Field missing';
+                }
+            }
+        }
+        foreach ($params as $k=>$v) {
+            $msg = null;
+            switch ($k) {
+                case 'firstName':
+                case 'lastName':
+                    if (empty($v)) {
+                        $msg = 'Cannot be blank';
+                    }
+                    break;
+                case 'phone':
+                    try {
+                        $this->stripPhone($v);
+                    } catch (\Exception $e) {
+                        $msg = 'Invalid phone number';
+                    }
+                    break;
+                case 'email':
+                    if (strpos($v, '@') === false) {
+                        $msg = 'Invalid email address';
+                    }
+                    break;
+                case 'addedBy':
+                    if (!$v instanceof User) {
+                        $msg = sprintf('addedBy must be instance of "%s"', User::class);
+                    }
+                    break;
+                default:
+                    $msg = 'Unknown key';
+            }
+            if ($msg !== null) {
+                $errors[] = new ValidationItem($k, $msg);
+            }
+        }
+
+        return $errors;
+    }
+
+    /**
+     * @param Customer $c
+     * @param array    $params
+     */
+    public function commitCustomer(Customer $c, array $params = []): void {
+        $valid = empty($this->validateParams($params));
+        if ($valid !== true) {
+            throw new \InvalidArgumentException('Params did not validate. Call validateParams first.');
+        }
+        foreach ($params as $k=>$v) {
+            switch ($k) {
+                case 'firstName':
+                    $c->setFirstName($v);
+                    break;
+                case 'lastName':
+                    $c->setLastName($v);
+                    break;
+                case 'phone':
+                    $c->setPhone($this->stripPhone($v));
+                    break;
+                case 'mobileConfirmed':
+                    $c->setMobileConfirmed($this->paramToBool($v));
+                    break;
+                case 'email':
+                    $c->setEmail($v);
+                    break;
+                case 'doNotContact':
+                    $c->setDoNotContact($this->paramToBool($v));
+                    break;
+                case 'deleted':
+                    $c->setDeleted($this->paramToBool($v));
+                    break;
+                case 'addedBy':
+                    $c->setAddedBy($v);
+                    break;
+            }
+        }
+
+        if ($c->getId() === null) {
+            $this->em->persist($c);
+        }
+        $this->em->beginTransaction();
+        try {
+            $this->em->flush();
+            $this->em->commit();
+        } catch (\Exception $e) {
+            $this->em->rollback();
+            throw new \RuntimeException('Caught exception during flush', 0, $e);
+        }
+    }
+
+    private function stripPhone(string $phone): string {
+        return $this->phoneValidator->clean($phone);
+    }
+
+    private function paramToBool($param): bool {
+        return ($param !== 'false' && $param == true);
+    }
+}
