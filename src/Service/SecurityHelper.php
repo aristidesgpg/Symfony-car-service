76195601084fb81fcb91477e20b9b90e10a85dda
Pony <Pony1125@outlook.com>
2020-10-21 10:22:28 -0400

    Add generate/validate token in securityHelper

--- src/Service/SecurityHelper.php
+++ src/Service/SecurityHelper.php
@@ -6,7 +6,7 @@ use App\Entity\User;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
-
+use Symfony\Component\DependencyInjection\ContainerInterface as Container;
 /**
  * Class SecurityHelper
  *
@@ -14,10 +14,22 @@ use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
  */
 class SecurityHelper {
     /**
+     * @var Container
+     */
+    private $container;
+
+    /**
+     * @var String
+     */
+    private $secret;
+
+    /**
      * SecurityHelper constructor.
      *
      */
-    public function __construct () {
+    public function __construct (Container $container) {
+        $this->container = $container;
+        $this->secret    = $this->container->getParameter('pass_phrase');
     }
 
     /**
@@ -37,5 +49,87 @@ class SecurityHelper {
         }
 
         return true;
+    }
+
+    /**
+     * @param String $text
+     * 
+     * @return boolean
+     */
+    public function base64UrlEncode(String $text)
+    {
+        return str_replace(
+            ['+', '/', '='],
+            ['-', '_', ''],
+            base64_encode($text)
+        );
+    }
+
+    /**
+     * @param String $email
+     * 
+     * @return String
+     */
+    public function generateToken(String $email){
+        // Create the token header
+        $header = json_encode([
+            'typ' => 'JWT',
+            'alg' => 'HS256'
+        ]);
+        // Create the token payload
+        $payload = json_encode([
+            'email' => $email,
+            'exp'   => date('Y-m-d H:i:s', strtotime('1 hour'))
+        ]);
+
+        // Encode Header
+        $base64UrlHeader = $this->base64UrlEncode($header);
+        // Encode Payload
+        $base64UrlPayload = $this->base64UrlEncode($payload);
+        // Create Signature Hash
+        $signature = hash_hmac('sha256', $base64UrlHeader . "." . $base64UrlPayload, $this->secret, true);
+        // Encode Signature to Base64Url String
+        $base64UrlSignature = $this->base64UrlEncode($signature);
+        // Create JWT
+        $jwt = $base64UrlHeader . "." . $base64UrlPayload . "." . $base64UrlSignature;
+
+        return $jwt;
+    }
+
+    /**
+     * @param String $jwt
+     * 
+     * @return String
+     */
+    public function validateToken(String $jwt){
+        // split the token
+        $tokenParts = explode('.', $jwt);
+        $header = base64_decode($tokenParts[0]);
+        $payload = base64_decode($tokenParts[1]);
+        $signatureProvided = $tokenParts[2];
+
+        // check the expiration time - note this will cause an error if there is no 'exp' claim in the token
+        $expiration   = date(json_decode($payload)->exp);
+        $now          = date('Y-m-d H:i:s');
+        $tokenExpired = $now > $expiration;
+
+        // build a signature based on the header and payload using the secret
+        $base64UrlHeader = $this->base64UrlEncode($header);
+        $base64UrlPayload = $this->base64UrlEncode($payload);
+        $signature = hash_hmac('sha256', $base64UrlHeader . "." . $base64UrlPayload, $this->secret, true);
+        $base64UrlSignature = $this->base64UrlEncode($signature);
+
+        // verify it matches the signature provided in the token
+        $signatureValid = ($base64UrlSignature === $signatureProvided);
+
+        if ($tokenExpired) {
+            return "Token has expired.\n";
+        }
+
+        if ($signatureValid) {
+            return "The signature is valid.\n";
+        } else {
+            return "The signature is NOT valid\n";
+        }
     }
 }
