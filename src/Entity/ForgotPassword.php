e796ff0f7247d5607435b6bb96b9138f962da202
Pony <Pony1125@outlook.com>
2020-10-28 14:19:22 -0400

    Add Entity

---
+++ src/Entity/ForgotPassword.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ForgotPasswordRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ForgotPasswordRepository::class)
+ */
+class ForgotPassword
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $user;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $token;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $expirationDate;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $used;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUser(): ?int
+    {
+        return $this->user;
+    }
+
+    public function setUser(int $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getToken(): ?string
+    {
+        return $this->token;
+    }
+
+    public function setToken(string $token): self
+    {
+        $this->token = $token;
+
+        return $this;
+    }
+
+    public function getExpirationDate(): ?\DateTimeInterface
+    {
+        return $this->expirationDate;
+    }
+
+    public function setExpirationDate(\DateTimeInterface $expirationDate): self
+    {
+        $this->expirationDate = $expirationDate;
+
+        return $this;
+    }
+
+    public function getUsed(): ?bool
+    {
+        return $this->used;
+    }
+
+    public function setUsed(bool $used): self
+    {
+        $this->used = $used;
+
+        return $this;
+    }
+}
