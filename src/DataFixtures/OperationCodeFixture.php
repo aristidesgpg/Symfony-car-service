2df76fb8edc8a2ef09f4c6f79328194702385e25
Pony <Pony1125@outlook.com>
2020-10-30 17:24:33 -0400

    Make an OperationCode DataFixture

--- src/DataFixtures/OperationCodeFixture.php
+++ src/DataFixtures/OperationCodeFixture.php
@@ -10,6 +10,24 @@ use App\Entity\OperationCode;
 class OperationCodeFixture extends Fixture
 {
     public function load(ObjectManager $manager) {
+        $faker = Factory::create();
 
+        for ($i = 1; $i <= 10; $i++) {
+            $operationCode = new OperationCode();
+
+            $operationCode->setCode($faker->unique()->word)
+                    ->setDescription($faker->sentence($nbWords = 6, $variableNbWords = true))
+                    ->setLaborHours($faker->randomFloat($nbMaxDecimals = NULL, $min = 0, $max = NULL))
+                    ->setLaborTaxable($faker->boolean(50))
+                    ->setPartsPrice($faker->randomFloat($nbMaxDecimals = NULL, $min = 0, $max = NULL))
+                    ->setPartsTaxable($faker->boolean(50))
+                    ->setSuppliesPrice($faker->randomFloat($nbMaxDecimals = NULL, $min = 0, $max = NULL))
+                    ->setSuppliesTaxable($faker->boolean(50));
+
+            $manager->persist($operationCode);
+            $manager->flush();
+
+            $this->addReference('OperationCode_' . $i, $operationCode);
+        }
     }
 }
