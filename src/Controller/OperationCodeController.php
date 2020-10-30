689b8851ed8756152253239e87b762d430c99c30
Pony <Pony1125@outlook.com>
2020-10-30 17:23:09 -0400

    Create a new operation code

--- src/Controller/OperationCodeController.php
+++ src/Controller/OperationCodeController.php
@@ -42,4 +42,116 @@ class OperationCodeController extends AbstractFOSRestController
 
         return $this->handleView($this->view($operationCodes, Response::HTTP_OK));
     }
+
+    /**
+     * @Rest\Post("/api/operaction-code")
+     *
+     * @SWG\Tag(name="OperationCode")
+     * @SWG\Post(description="Create a New Operation Code")
+     * 
+     * @SWG\Parameter(
+     *     name="code",
+     *     in="query",
+     *     required=true,
+     *     type="string",
+     *     description="Operation Code"
+     * )
+     * @SWG\Parameter(
+     *     name="description",
+     *     in="formData",
+     *     required=true,
+     *     type="string",
+     *     description="The Description for Operation Code",
+     * )
+     * @SWG\Parameter(
+     *     name="laborHours",
+     *     in="formData",
+     *     required=true,
+     *     type="number",
+     *     description="The Labor Hours",
+     * )
+     * @SWG\Parameter(
+     *     name="laborTaxable",
+     *     in="formData",
+     *     required=true,
+     *     type="boolean",
+     *     description="The Labor Taxable",
+     * )
+     * @SWG\Parameter(
+     *     name="partsPrice",
+     *     in="formData",
+     *     required=true,
+     *     type="number",
+     *     description="The Parts Price",
+     * )
+     * @SWG\Parameter(
+     *     name="partsTaxable",
+     *     in="formData",
+     *     required=true,
+     *     type="boolean",
+     *     description="The Parts Taxable",
+     * )
+     * @SWG\Parameter(
+     *     name="suppliesPrice",
+     *     in="formData",
+     *     required=true,
+     *     type="number",
+     *     description="The Supplies Price",
+     * )
+     * @SWG\Parameter(
+     *     name="suppliesTaxable",
+     *     in="formData",
+     *     required=true,
+     *     type="boolean",
+     *     description="The Supplies Taxable",
+     * )
+     * 
+     * @SWG\Response(
+     *     response=200,
+     *     description="Return status code",
+     *     @SWG\Items(
+     *         type="object",
+     *             @SWG\Property(property="status", type="string", description="status code", example={"status":
+     *                                              "Successfully created" }),
+     *         )
+     * )
+     *
+     * @param Request                 $request
+     * @param OperationCodeRepository $operationCodeRepo
+     * 
+     * @return Response
+     */
+    public function create (Request $request, EntityManagerInterface $em) {
+        $code            = $request->get('code'); 
+        $description     = $request->get('description');
+        $laborHours      = $request->get('laborHours');
+        $laborTaxable    = $request->get('laborTaxable');
+        $partsPrice      = $request->get('partsPrice');
+        $partsTaxable    = $request->get('partsTaxable');
+        $suppliesPrice   = $request->get('suppliesPrice');
+        $suppliesTaxable = $request->get('suppliesTaxable');
+
+        //params are invalid
+        if(!$code || !$description || !$laborHours || !$laborTaxable || !$partsPrice || !$partsTaxable || !$suppliesPrice || !$suppliesTaxable){
+            return $this->handleView($this->view('Missing Required Parameter', Response::HTTP_BAD_REQUEST));
+        }
+
+        //create a new operation code
+        $operationCode = new OperationCode();
+        $operationCode->setCode($code)
+                      ->setDescription($description)
+                      ->setLaborHours($laborHours)
+                      ->setLaborTaxable($laborTaxable)
+                      ->setPartsPrice($partsPrice)
+                      ->setPartsTaxable($partsTaxable)
+                      ->setSuppliesPrice($suppliesPrice)
+                      ->setSuppliesTaxable($suppliesTaxable);
+
+        $em->persist($operationCode);
+        $em->flush();
+
+        return $this->handleView($this->view([
+            'message' => 'Operation Code Created'
+        ], Response::HTTP_OK));
+    }
 }
