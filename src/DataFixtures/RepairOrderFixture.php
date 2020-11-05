3af660044763369b6a63340897f555994c9f0880
Laramie Rugen <ffgpga08@gmail.com>
2020-11-05 15:08:21 -0500

    Updated RO fixture to randomize some more data

--- src/DataFixtures/RepairOrderFixture.php
+++ src/DataFixtures/RepairOrderFixture.php
@@ -53,10 +53,46 @@ class RepairOrderFixture extends Fixture implements DependentFixtureInterface {
 
         // Now make 150 more
         for ($i = 1; $i <= 150; $i++) {
-            $repairOrder       = new RepairOrder();
-            $userReference     = $faker->numberBetween(1, 50);
-            $customerReference = $faker->numberBetween(1, 50);
-            $advisorReference  = $faker->numberBetween(1, 30);
+            $repairOrder         = new RepairOrder();
+            $userReference       = $faker->numberBetween(1, 50);
+            $customerReference   = $faker->numberBetween(1, 50);
+            $advisorReference    = $faker->numberBetween(1, 30);
+            $waiter              = $faker->boolean(75);
+            $pickupDate          = null;
+            $dateCreated         = $faker->dateTimeBetween('-1 year');
+            $dateClosed          = clone $dateCreated;
+            $dateModify          = random_int(1, 12);
+            $dateClosed          = $dateClosed->modify('+' . $dateModify . ' hours');
+            $closedRandom        = random_int(0, 100);
+            $startValue          = $faker->randomFloat(2, 100, 3000);
+            $approvedValue       = null;
+            $finalValue          = null;
+            $approvedValueRandom = random_int(0, 100);
+
+            if (!$waiter) {
+                $pickupDate = clone $dateCreated;
+                $pickupDate->modify('+' . $faker->numberBetween(1, 24) . ' hours');
+            }
+
+            // 80% chance it's closed
+            if ($closedRandom > 80) {
+                $dateClosed = null;
+            }
+
+            if ($dateClosed) {
+                $finalValue = $startValue + $faker->randomFloat(2, 0, 1000);
+            }
+
+            // 50% chance if open to have approved, or if it is closed already, set approved value
+            if ($approvedValueRandom > 50 || $dateClosed) {
+                $approvedMax = 1000;
+                if ($finalValue) {
+                    $approvedMax = $finalValue - $startValue;
+                }
+
+                $approvedValue = $startValue + $faker->randomFloat(2, 0, $approvedMax);
+            }
+
             $repairOrder->setNumber($faker->unique(true)->numberBetween(100000, 999999))
                         ->setPrimaryCustomer($this->getReference('customer_' . $customerReference))
                         ->setPrimaryTechnician($this->getReference('user_' . $userReference))
@@ -65,10 +101,22 @@ class RepairOrderFixture extends Fixture implements DependentFixtureInterface {
                         ->setMpiStatus($statusOptions[$faker->numberBetween(0, 5)])
                         ->setPaymentStatus($statusOptions[$faker->numberBetween(0, 5)])
                         ->setQuoteStatus($statusOptions[$faker->numberBetween(0, 5)])
-                        ->setWaiter($faker->boolean(25))
+                        ->setWaiter($waiter)
+                        ->setPickupDate($pickupDate)
                         ->setLinkHash(sha1($faker->unique(true)->randomAscii))
                         ->setDeleted($faker->boolean(2))
-                        ->setArchived($faker->boolean(5));
+                        ->setArchived($faker->boolean(5))
+                        ->setDateCreated($dateCreated)
+                        ->setDateClosed($dateClosed)
+                        ->setInternal($faker->boolean(1))
+                        ->setYear($faker->year)
+                        ->setMake('Toyota')
+                        ->setModel('Corolla')
+                        ->setMiles($faker->numberBetween(1000, 250000))
+                        ->setVin(sha1($faker->unique(true)->randomAscii))
+                        ->setStartValue($startValue)
+                        ->setApprovedValue($approvedValue)
+                        ->setFinalValue($finalValue);
 
             $manager->persist($repairOrder);
             $manager->flush();
