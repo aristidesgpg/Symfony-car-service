feb7c20253dbce7c92703c99879bbf9267190e30
Tyler Holcomb <tyler@tholcomb.com>
2020-11-11 21:28:14 -0500

    Added KnpPaginatorBundle

--- config/bundles.php
+++ config/bundles.php
@@ -19,4 +19,5 @@ return [
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
 ];
