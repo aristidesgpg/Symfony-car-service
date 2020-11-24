a8d47a1053b63fc2b8b86093cff7c0d44e195278
Pony <Pony1125@outlook.com>
2020-11-24 12:57:33 -0500

    Update/Delete repairOrderQuote endpoint

--- src/Controller/RepairOrderQuoteController.php
+++ src/Controller/RepairOrderQuoteController.php
@@ -141,4 +141,126 @@ class RepairOrderQuoteController extends AbstractFOSRestController {
             'message' => 'RepairOrderQuote Created'
         ], Response::HTTP_OK));
     }
+
+    /**
+     * @Rest\Put("/api/repair-order-quote/{id}")
+     *
+     * @SWG\Tag(name="Repair Order Quote")
+     * @SWG\Put(description="Update a Repair Order Quote")
+     *
+     * @SWG\Parameter(
+     *     name="repair_order",
+     *     in="formData",
+     *     required=true,
+     *     type="integer",
+     *     description="The Repair Order ID",
+     * )
+     * @SWG\Parameter(
+     *     name="date_sent",
+     *     in="formData",
+     *     required=true,
+     *     type="string",
+     *     description="The Sent Date",
+     * )
+     * @SWG\Parameter(
+     *     name="date_customer_viewed",
+     *     in="formData",
+     *     required=true,
+     *     type="string",
+     *     description="The Customer Viewed Date",
+     * )
+     * @SWG\Parameter(
+     *     name="date_customer_completed",
+     *     in="formData",
+     *     required=true,
+     *     type="string",
+     *     description="The Customer Completed Date",
+     * )
+     * @SWG\Parameter(
+     *     name="date_completed_viewed",
+     *     in="formData",
+     *     required=true,
+     *     type="string",
+     *     description="The Completed Viewed Date",
+     * )
+     * 
+     * @SWG\Response(
+     *     response=200,
+     *     description="Return status code",
+     *     @SWG\Items(
+     *         type="object",
+     *             @SWG\Property(property="status", type="string", description="status code", example={"status":
+     *                                              "Successfully Updated" }),
+     *         )
+     * )
+     *
+     * @param RepairOrderQuote       $repairOrderQuote
+     * @param Request                $request
+     * @param RepairOrderRepository  $repairOrderRepository
+     * @param EntityManagerInterface $em
+     *
+     * @return Response
+     */
+    public function updateRepairOrderQuote (RepairOrderQuote $repairOrderQuote, Request $request, RepairOrderRepository $repairOrderRepository, EntityManagerInterface $em) {
+        $repairOrderID         = $request->get('repair_order');
+        $dateSent              = $request->get('date_sent');
+        $dateCustomerViewed    = $request->get('date_customer_viewed');
+        $dateCustomerCompleted = $request->get('date_customer_completed');
+        $dateCompletedViewed   = $request->get('date_completed_viewed');
+        //check if params are valid
+        if(!$repairOrderID || !$dateSent || !$dateCustomerViewed || !$dateCustomerCompleted || !$dateCompletedViewed){
+            return $this->handleView($this->view('Missing Required Parameter', Response::HTTP_BAD_REQUEST));
+        }
+        //Check if Repair Order exists
+        $repairOrder  = $repairOrderRepository->find($repairOrderID);
+        if (!$repairOrder) {
+            return $this->handleView($this->view('Invalid repair_order Parameter', Response::HTTP_BAD_REQUEST));
+        }
+        //update repairOrderQuote
+        $repairOrderQuote->setRepairOrder($repairOrder)
+                         ->setDateSent($dateSent)
+                         ->setDateCustomerViewed($dateCustomerViewed)
+                         ->setDateCustomerCompleted($dateCustomerCompleted)
+                         ->setDateCompletedViewed($dateCompletedViewed);
+
+        $em->persist($repairOrderQuote);
+        $em->flush();
+
+        return $this->handleView($this->view([
+            'message' => 'RepairOrderQuote Updated'
+        ], Response::HTTP_OK));
+    }
+
+    /**
+     * @Rest\Put("/api/repair-order-quote/{id}")
+     *
+     * @SWG\Tag(name="Repair Order Quote")
+     * @SWG\Put(description="Delete a Repair Order Quote")
+     * 
+     * @SWG\Response(
+     *     response=200,
+     *     description="Return status code",
+     *     @SWG\Items(
+     *         type="object",
+     *             @SWG\Property(property="status", type="string", description="status code", example={"status":
+     *                                              "Successfully Deleted" }),
+     *         )
+     * )
+     *
+     * @param RepairOrderQuote       $repairOrderQuote
+     * @param EntityManagerInterface $em
+     *
+     * @return Response
+     */
+    public function deleteRepairOrderQuote (RepairOrderQuote $repairOrderQuote, EntityManagerInterface $em) {
+        //delete repairOrderQuote
+        $repairOrderQuote->setDeleted(true);
+
+        $em->persist($repairOrderQuote);
+        $em->flush();
+
+        return $this->handleView($this->view([
+            'message' => 'RepairOrderQuote Deleted'
+        ], Response::HTTP_OK));
+    }
 }
