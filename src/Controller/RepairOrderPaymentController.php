1ead863a1f9e6442a06cc5a803ec2af84439527b
Tyler Holcomb <tyler@tholcomb.com>
2020-11-24 17:22:56 -0500

    Added RepairOrderPaymentController & PaymentHelper (WIP)

--- src/Controller/RepairOrderPaymentController.php
+++ src/Controller/RepairOrderPaymentController.php
@@ -6,19 +6,14 @@ use App\Entity\RepairOrder;
 use App\Entity\RepairOrderPayment;
 use App\Exception\PaymentException;
 use App\Helper\FalsyTrait;
-use App\Money\MoneyHelper;
-use App\Response\ValidationResponse;
 use App\Service\PaymentHelper;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
-use Money\Exception\ParserException;
-use Money\Money;
 use Nelmio\ApiDocBundle\Annotation\Model;
 use Swagger\Annotations as SWG;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Twilio\Exceptions\TwilioException;
 
 /**
  * Class RepairOrderPaymentController
@@ -74,30 +69,15 @@ class RepairOrderPaymentController extends AbstractFOSRestController {
      * )
      * @SWG\Response(response="406", ref="#/responses/ValidationResponse")
      *
-     * @SWG\Parameter(name="amount", type="string", in="formData", required=true)
-     *
-     * @param RepairOrder   $ro
-     * @param Request       $request
-     * @param PaymentHelper $helper
+     * @param RepairOrder $ro
+     * @param Request     $request
      *
      * @return Response
      */
-    public function createPayment (RepairOrder $ro, Request $request, PaymentHelper $helper): Response {
+    public function createPayment (RepairOrder $ro, Request $request): Response {
         if ($ro->getDeleted()) {
             throw new NotFoundHttpException();
         }
-
-        try {
-            $amount = $this->parseAmount($request->request->get('amount'));
-        } catch (\InvalidArgumentException $e) {
-            return new ValidationResponse(['amount' => $e->getMessage()]);
-        }
-
-        $payment = $helper->addPayment($ro, $amount);
-        $view = $this->view($payment);
-        $view->getContext()->setGroups(RepairOrderPayment::GROUPS);
-
-        return $this->handleView($view);
     }
 
     /**
@@ -131,7 +111,6 @@ class RepairOrderPaymentController extends AbstractFOSRestController {
      *
      * @param RepairOrder        $ro
      * @param RepairOrderPayment $payment
-     * @param PaymentHelper      $helper
      *
      * @return Response
      */
@@ -157,7 +136,6 @@ class RepairOrderPaymentController extends AbstractFOSRestController {
      * @Rest\Post("/{payment}/send")
      * @SWG\Response(response="200", description="Success!")
      * @SWG\Response(response="400", description="Payment already sent")
-     * @SWG\Response(response="500", description="Unable to send payment")
      *
      * @SWG\Parameter(name="resend", type="boolean", in="formData", description="Resend payment")
      *
@@ -181,17 +159,7 @@ class RepairOrderPaymentController extends AbstractFOSRestController {
             ], Response::HTTP_BAD_REQUEST));
         }
 
-        try {
-            $helper->sendPayment($payment);
-        } catch (TwilioException $e) {
-            return $this->handleView($this->view([
-                'message' => 'Unable to send payment',
-            ], Response::HTTP_INTERNAL_SERVER_ERROR));
-        }
-
-        return $this->handleView($this->view([
-            'message' => 'Payment sent',
-        ]));
+        // TODO: Send payment
     }
 
     /**
@@ -208,9 +176,6 @@ class RepairOrderPaymentController extends AbstractFOSRestController {
         if ($ro->getDeleted() || $payment->isDeleted() || $ro !== $payment->getRepairOrder()) {
             throw new NotFoundHttpException();
         }
-
-        $paid = ($payment->getDatePaid() !== null);
-        $helper->viewPayment($payment, $paid);
 
         return $this->handleView($this->view([
             'message' => 'Payment view recorded',
@@ -292,94 +257,5 @@ class RepairOrderPaymentController extends AbstractFOSRestController {
         if ($ro->getDeleted() || $payment->isDeleted() || $ro !== $payment->getRepairOrder()) {
             throw new NotFoundHttpException();
         }
-
-        try {
-            $amount = $this->parseAmount($request->request->get('amount'));
-        } catch (\InvalidArgumentException $e) {
-            return new ValidationResponse(['amount' => $e->getMessage()]);
-        }
-
-        $code = Response::HTTP_OK;
-        $message = null;
-        try {
-            $helper->refundPayment($payment, $amount);
-            $message = 'Refund successful';
-        } catch (\InvalidArgumentException|PaymentException $e) {
-            $message = $e->getMessage();
-            $code = Response::HTTP_BAD_REQUEST;
-        } catch (\Exception $e) {
-            $message = 'Could not contact payment server';
-            $code = Response::HTTP_INTERNAL_SERVER_ERROR;
-        }
-
-        return $this->handleView($this->view([
-            'message' => $message,
-        ], $code));
-    }
-
-    /**
-     * @Rest\Post("/{payment}/sendReceipt")
-     * @SWG\Response(response="200", description="Success!")
-     * @SWG\Response(response="400", description="Payment has not been paid")
-     * @SWG\Response(response="500", description="Could not send mail")
-     *
-     * @SWG\Parameter(name="sendTo", type="string", in="formData", required=true)
-     *
-     * @param RepairOrder        $ro
-     * @param RepairOrderPayment $payment
-     * @param Request            $request
-     * @param PaymentHelper      $helper
-     *
-     * @return Response
-     */
-    public function sendReceipt (RepairOrder $ro, RepairOrderPayment $payment, Request $request,
-                                 PaymentHelper $helper): Response {
-        if ($ro->getDeleted() || $payment->isDeleted() || $ro !== $payment->getRepairOrder()) {
-            throw new NotFoundHttpException();
-        }
-
-        $text = null;
-        if ($payment->getDatePaid() === null) {
-            $text = 'Cannot send receipt for unpaid order';
-        } elseif (!$request->request->has('sendTo')) {
-            $text = 'Missing sendTo parameter';
-        }
-        if ($text !== null) {
-            return $this->handleView($this->view([
-                'message' => $text,
-            ], Response::HTTP_BAD_REQUEST));
-        }
-
-        $text = 'Receipt sent';
-        $code = Response::HTTP_OK;
-        try {
-            $helper->sendReceipt($payment, $request->request->get('sendTo'));
-        } catch (\Throwable $e) {
-            $text = 'Could not send receipt';
-            $code = Response::HTTP_INTERNAL_SERVER_ERROR;
-        }
-
-        return $this->handleView($this->view([
-            'message' => $text,
-        ], $code));
-    }
-
-    /**
-     * @param string|null $amount
-     *
-     * @return Money
-     * @throws \InvalidArgumentException
-     */
-    private function parseAmount (?string $amount): Money {
-        try {
-            $money = MoneyHelper::parse($amount);
-        } catch (ParserException $e) {
-            throw new \InvalidArgumentException('Invalid format', 0, $e);
-        }
-        if ($money->isNegative() || $money->isZero()) {
-            throw new \InvalidArgumentException('Must be greater than zero');
-        }
-
-        return $money;
     }
 }
--- src/Service/PaymentHelper.php
+++ src/Service/PaymentHelper.php
@@ -2,108 +2,18 @@
 
 namespace App\Service;
 
-use App\Entity\RepairOrder;
 use App\Entity\RepairOrderPayment;
 use App\Entity\RepairOrderPaymentInteraction;
 use App\Exception\PaymentException;
-use App\Money\MoneyHelper;
-use App\Repository\SettingsRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Money\Money;
-use Twilio\Exceptions\TwilioException;
 
 class PaymentHelper {
-    /** @var EntityManagerInterface */
     private $em;
-
-    /** @var NMI */
     private $nmi;
 
-    /** @var ShortUrlHelper */
-    private $urlHelper;
-
-    /** @var SettingsRepository */
-    private $settings;
-
-    /** @var MailerHelper */
-    private $mailer;
-
-    /**
-     * PaymentHelper constructor.
-     * @param EntityManagerInterface $em
-     * @param NMI                    $nmi
-     * @param ShortUrlHelper         $urlHelper
-     * @param SettingsRepository     $settings
-     * @param MailerHelper           $mailer
-     */
-    public function __construct (
-        EntityManagerInterface $em,
-        NMI $nmi,
-        ShortUrlHelper $urlHelper,
-        SettingsRepository $settings,
-        MailerHelper $mailer
-    ) {
+    public function __construct (EntityManagerInterface $em, NMI $nmi) {
         $this->em  = $em;
         $this->nmi = $nmi;
-        $this->urlHelper = $urlHelper;
-        $this->settings = $settings;
-        $this->mailer = $mailer;
-    }
-
-    /**
-     * @param RepairOrder $ro
-     * @param Money       $amount
-     *
-     * @return RepairOrderPayment
-     */
-    public function addPayment (RepairOrder $ro, Money $amount): RepairOrderPayment {
-        $payment = new RepairOrderPayment();
-        $payment->setRepairOrder($ro)
-                ->setAmount($amount)
-                ->setDateCreated(new \DateTime());
-        $this->createInteraction($payment, 'Created');
-        $this->commitPayment($payment);
-
-        return $payment;
-    }
-
-    /**
-     * @param RepairOrderPayment $payment
-     * @param bool               $resend
-     *
-     * @throws TwilioException
-     */
-    public function sendPayment (RepairOrderPayment $payment, bool $resend = false): void {
-        if ($payment->getDateSent() !== null && $resend !== true) {
-            throw new \InvalidArgumentException('Payment already sent');
-        }
-
-        $url = ''; // TODO
-        $message = $this->settings->find('serviceTextPayment')->getValue();
-        $phone = $payment->getRepairOrder()->getPrimaryCustomer()->getPhone();
-
-        $this->urlHelper->sendShortenedLink($phone, $message, $url);
-
-        $payment->setDateSent(new \DateTime());
-        $this->createInteraction($payment, 'Sent');
-        $this->commitPayment($payment);
-    }
-
-    /**
-     * @param RepairOrderPayment $payment
-     * @param bool               $paid
-     */
-    public function viewPayment (RepairOrderPayment $payment, bool $paid = false): void {
-        $date = new \DateTime();
-        if ($paid) {
-            $payment->setDatePaidViewed($date);
-            $interaction = 'Paid Viewed';
-        } else {
-            $payment->setDateViewed($date);
-            $interaction = 'Viewed';
-        }
-        $this->createInteraction($payment, $interaction);
-        $this->commitPayment($payment);
     }
 
     /**
@@ -113,56 +23,10 @@ class PaymentHelper {
      * @throws \Exception|PaymentException
      */
     public function payPayment (RepairOrderPayment $payment, string $paymentToken): void {
-        $response      = $this->nmi->makePayment($paymentToken, $payment->getAmountString())->getParsedResponse();
+        $response      = $this->nmi->makePayment($paymentToken, $payment->getAmount())->getParsedResponse();
         $transactionId = $response['transaction_id'] ?? null;
-        $payment->setTransactionId($transactionId);
-        $payment->setDatePaid(new \DateTime());
-        $this->createInteraction($payment, 'Paid');
-
-        try {
-            $lookup = $this->nmi->lookupTransaction($transactionId);
-            $payment->setCardType($lookup['transaction']['cc_type']);
-            $payment->setCardNumber($lookup['transaction']['cc_number']);
-        } catch (\Exception $e) {
-            // TODO: Fail gracefully
-        } finally {
-            $this->commitPayment($payment);
-        }
     }
 
-    /**
-     * @param RepairOrderPayment $payment
-     * @param Money              $amount
-     *
-     * @throws \Exception|PaymentException
-     */
-    public function refundPayment (RepairOrderPayment $payment, Money $amount): void {
-        $transactionId = $payment->getTransactionId();
-        if ($transactionId === null) {
-            throw new \InvalidArgumentException('Missing transactionId');
-        }
-        $overflow = $amount->greaterThan($payment->getAmount());
-        if ($payment->getRefundedAmount() !== null) {
-            $totalRefunded = $payment->getRefundedAmount()->add($amount);
-            $overflow = $totalRefunded->greaterThan($payment->getAmount());
-        }
-        if ($overflow === true) {
-            throw new \InvalidArgumentException(sprintf('Refund amount "%s" exceeds total amount "%s"',
-                MoneyHelper::getFormatter()->format($amount),
-                MoneyHelper::getFormatter()->format($payment->getAmount())
-            ));
-        }
-
-        $this->nmi->makeRefund($transactionId, MoneyHelper::getFormatter()->format($amount));
-        $payment->setDateRefunded(new \DateTime())
-                ->setRefundedAmount(isset($totalRefunded) ? $totalRefunded : $amount);
-        $this->createInteraction($payment, 'Refunded');
-        $this->commitPayment($payment);
-    }
-
-    /**
-     * @param RepairOrderPayment $payment
-     */
     public function deletePayment (RepairOrderPayment $payment): void {
         if ($payment->getDatePaid() !== null) {
             throw new \InvalidArgumentException("Paid payments cannot be deleted");
@@ -173,59 +37,14 @@ class PaymentHelper {
 
         $payment->setDeleted(true);
         $payment->setDateDeleted(new \DateTime());
-        $this->createInteraction($payment, 'Deleted');
-
-        $this->commitPayment($payment);
-    }
-
-    /**
-     * @param RepairOrderPayment $payment
-     * @param string             $toAddress
-     *
-     * @throws \Throwable
-     */
-    public function sendReceipt (RepairOrderPayment $payment, string $toAddress): void {
-        $date = $payment->getDatePaid();
-        if ($date === null) {
-            throw new \InvalidArgumentException('Cannot send receipt for unpaid payment');
-        }
-
-        $dealerName = $this->settings->find('generalName')->getValue();
-        $emailBody = $this->mailer->renderEmail('email-payment-receipt.html.twig', [
-           'dealer_name' => $dealerName,
-           'customer_phone' => $payment->getRepairOrder()->getPrimaryCustomer()->getPhone(),
-           'transaction_id' => $payment->getTransactionId(),
-           'ro_number' => $payment->getRepairOrder()->getNumber(),
-           'date' => $date,
-           'amount' => $payment->getAmountString(),
-        ]);
-
-        if (!$this->mailer->sendHtmlMail("iService Auto {$dealerName} Payment Receipt", $toAddress, $emailBody)) {
-            throw new \RuntimeException('Could not send email');
-        }
-
-        $this->createInteraction($payment, 'Receipt sent');
-        $this->commitPayment($payment);
-    }
-
-    /**
-     * @param RepairOrderPayment $payment
-     * @param string             $type
-     *
-     * @return RepairOrderPaymentInteraction
-     */
-    private function createInteraction (RepairOrderPayment $payment, string $type): RepairOrderPaymentInteraction {
         $interaction = new RepairOrderPaymentInteraction();
-        $interaction->setPayment($payment)
-                    ->setType($type);
+        $interaction->setPayment($payment);
+        $interaction->setType('Deleted');
         $payment->addInteraction($interaction);
 
-        return $interaction;
+        $this->commitPayment($payment);
     }
 
-    /**
-     * @param RepairOrderPayment $payment
-     */
     private function commitPayment (RepairOrderPayment $payment): void {
         if ($payment->getId() === null) {
             $this->em->persist($payment);
