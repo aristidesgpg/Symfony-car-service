e25bfed893d93bb8ae3de233a3bdbaf4f28c06d2
Pony <Pony1125@outlook.com>
2020-12-01 11:20:44 -0500

    Add processRefund and shareRepairOrders

---
+++ migrations/Version20201201154959.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20201201154959 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user ADD process_refund TINYINT(1) NOT NULL, ADD share_repair_orders TINYINT(1) NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user DROP process_refund, DROP share_repair_orders');
+    }
+}
