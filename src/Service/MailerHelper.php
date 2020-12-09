dc6ada7fb1dd4aaa90a97a4afb41fa60b3475288
Tyler Holcomb <tyler@tholcomb.com>
2020-12-09 18:58:13 -0500

    Added sendHtmlMail & renderEmail

--- src/Service/MailerHelper.php
+++ src/Service/MailerHelper.php
@@ -5,6 +5,8 @@ namespace App\Service;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface as Container;
+use TijsVerkoyen\CssToInlineStyles\CssToInlineStyles;
+use Twig\Environment;
 
 /**
  * Class MailerHelper
@@ -28,10 +30,16 @@ class MailerHelper {
      */
     private $sender;
 
-    public function __construct (Container $container, \Swift_Mailer $mailer) {
+    /**
+     * @var Environment
+     */
+    private $twig;
+
+    public function __construct (Container $container, \Swift_Mailer $mailer, Environment $twig) {
         $this->container = $container;
         $this->sender    = $this->container->getParameter('sender');
         $this->mailer    = $mailer;
+        $this->twig = $twig;
     }
 
     /**
@@ -56,5 +64,71 @@ class MailerHelper {
         } 
         
         return true;
+    }
+
+    /**
+     * @param string       $subject
+     * @param string       $address
+     * @param string|array $body
+     *
+     * @return bool
+     */
+    public function sendHtmlMail(string $subject, string $address, $body): bool {
+        if (is_string($body)) {
+            $body = ['html' => $body];
+        } elseif (is_array($body)) {
+            if (!isset($body['html']) || empty($body['html'])) {
+                throw new \InvalidArgumentException('$body missing html');
+            }
+        } else {
+            throw new \InvalidArgumentException('$body must be array or string');
+        }
+        $message = (new \Swift_Message($subject))
+            ->setFrom($this->sender)
+            ->setTo($address)
+            ->setBody($body['html'], 'text/html');
+        if (isset($body['text']) && $body['text'] !== null) {
+            $message->addPart($body['text'], 'text/plain');
+        }
+
+        return ($this->mailer->send($message) !== 0);
+    }
+
+    /**
+     * @param string $template
+     * @param array  $params
+     *
+     * @return array
+     * @throws \Throwable
+     */
+    public function renderEmail(string $template, array $params = []): array {
+        $return = [
+            'html' => null,
+            'text' => null,
+        ];
+        $template = $this->twig->resolveTemplate($template);
+        if ($template->hasBlock('text')) {
+            $return['text'] = $this->htmlToPlaintext($template->renderBlock('text', $params));
+        }
+        $html = $template->render($params);
+        $css = file_get_contents(__DIR__ . '/../../assets/foundation-emails.css');
+        $return['html'] = (new CssToInlineStyles())->convert($html, $css);
+
+        return $return;
+    }
+
+    /**
+     * @param string $html
+     *
+     * @return string
+     */
+    private function htmlToPlaintext(string $html): string {
+        $html = strip_tags($html);
+        $parts = explode("\n", $html);
+        foreach ($parts as &$p) {
+            $p = trim($p);
+        }
+
+        return implode("\n", $parts);
     }
 }
