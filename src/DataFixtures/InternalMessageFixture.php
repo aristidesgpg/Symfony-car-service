43aac52b7b39609428d9813a2639001a33ba27e0
gokhanmelma <gokhanmelma@gmail.com>
2020-12-14 08:05:39 -0500

    Add data fixture for InternalMessage

---
+++ src/DataFixtures/InternalMessageFixture.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\InternalMessage;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+class InternalMessageFixture extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $faker = Factory::create();
+
+        for ($i = 1; $i <= 50; $i++) {
+            $internalMessage = new InternalMessage();
+            $userReferenceFrom   = $faker->numberBetween(1, 50);
+            $userReferenceTo   = $faker->numberBetween(1, 50);
+
+            if ($userReferenceFrom === $userReferenceTo) {
+                $userReferenceTo = $userReferenceFrom < 50 ? $userReferenceFrom + 1 : 1;
+            }
+        
+            $internalMessage->setFrom($this->getReference('user_' . $userReferenceFrom))
+                        ->setTo($this->getReference('user_' . $userReferenceTo))
+                        ->setMessage($faker->sentence())
+                        ->setDate($faker->dateTimeBetween('-1 year'))
+                        ->setIsRead($faker->boolean(50));
+        
+            $manager->persist($internalMessage);
+            $manager->flush();
+        }
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getDependencies()
+    {
+        return [ UserFixture::class ];
+    }
+}
