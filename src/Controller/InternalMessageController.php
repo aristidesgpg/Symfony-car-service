872c89e5094bf08a38f5c4ecd3ebe8834a9fbc97
gokhanmelma <gokhanmelma@gmail.com>
2020-12-15 20:07:30 -0500

    Update get threads endpoint

--- src/Controller/InternalMessageController.php
+++ src/Controller/InternalMessageController.php
@@ -24,10 +24,17 @@ class InternalMessageController extends AbstractFOSRestController
     private const PAGE_LIMIT = 25;
 
     /**
-     * @Rest\Get("/api/internal-messaging")
+     * @Rest\Get("/api/internal-messaging/conversations")
+     * @Rest\Get(name="getInternalConversations")
      * 
      * @SWG\Tag(name="Internal Message")
-     * @SWG\Get(description="Get the list of conversations of the logged in user")
+     * @SWG\Get(description="Get the list of conversations between multiple customers")
+     * @SWG\Parameter(
+     *     name="page",
+     *     required=false,
+     *     type="integer",
+     *     in="query"
+     * )
      * @SWG\Response(
      *      response=200,
      *      description="Return the list of conversations",
@@ -39,21 +46,60 @@ class InternalMessageController extends AbstractFOSRestController
      *      )
      * )
      * 
+     * @param Request                   $request
+     * @param InternalMessageRepository $internalMessageRepository
+     * @param PaginatorInterface        $paginator
+     * @param UrlGeneratorInterface     $urlGenerator
      * 
      * @return Response
      */
-    public function getConversations()
+    public function getConversations(Request $request, InternalMessageRepository $internalMessageRepository, PaginatorInterface $paginator, UrlGeneratorInterface $urlGenerator)
     {
-        $user = $this->getUser();
+        $user       = $this->getUser();
+        $page       = $request->query->getInt('page', 1);
 
+        if ($page < 1) {
+            throw new NotFoundHttpException();
+        }
+
+        $allOpponents = $internalMessageRepository->createQueryBuilder('im')
+                                                  ->select('im.from')
+                                                  ->distinct()
+                                                  ->where('im.to = :userId')
+                                                  ->setParameter('userId', $user->getId())
+                                                  ->getQuery();
+        return $this->handleView($this->view($allOpponents->getResult()));
+
+        $urlParams  = $queryParams  = ['userId' => $user->getId()];
+        $query      = $internalMessageRepository->createQueryBuilder('im')
+                                           ->where('im.to = :userId')
+                                           ->andWhere('im.from = :opponentUserId')
+                                           ->setParameters($queryParams)
+                                           ->orderBy('im.date', 'DESC')
+                                           ->getQuery();
+
+        $pager      = $paginator->paginate($query, $page, self::PAGE_LIMIT);
+        $pagination = new Pagination($pager, self::PAGE_LIMIT, $urlGenerator);
+
+        $view       = $this->view([
+            'internalMessages'  => $pager->getItems(),
+            'totalResults'      => $pagination->totalResults,
+            'totalPages'        => $pagination->totalPages,
+            'previous'          => $pagination->getPreviousPageURL('getInternalMessages', $urlParams),
+            'currentPage'       => $pagination->currentPage,
+            'next'              => $pagination->getNextPageURL('getInternalMessages', $urlParams)
+        ]);
+        $view->getContext()->setGroups(['internal_message']);
+
+        return $this->handleView($view);
     }
 
     /**
-     * @Rest\Get("/api/internal-messaging/newest")
+     * @Rest\Get("/api/internal-messaging/messages")
      * @Rest\Get(name="getInternalMessages")
      * 
      * @SWG\Tag(name="Internal Message")
-     * @SWG\Get(description="Get the list of newest conversations")
+     * @SWG\Get(description="Get the list of messages in a conversation")
      * @SWG\Parameter(
      *     name="page",
      *     required=false,
@@ -68,7 +114,7 @@ class InternalMessageController extends AbstractFOSRestController
      * )
      * @SWG\Response(
      *      response=200,
-     *      description="Return the list of conversations",
+     *      description="Return the list of messages newest",
      *      @SWG\Schema(
      *          type="object",
      *          @SWG\Property(
@@ -91,7 +137,7 @@ class InternalMessageController extends AbstractFOSRestController
      * 
      * @return Response
      */
-    public function getConversationsNewest(Request $request, InternalMessageRepository $internalMessageRepository, PaginatorInterface $paginator, UrlGeneratorInterface $urlGenerator)
+    public function getMessagesNewest(Request $request, InternalMessageRepository $internalMessageRepository, PaginatorInterface $paginator, UrlGeneratorInterface $urlGenerator)
     {
         $user           = $this->getUser();
         $page           = $request->query->getInt('page', 1);
@@ -101,18 +147,18 @@ class InternalMessageController extends AbstractFOSRestController
             throw new NotFoundHttpException();
         }
 
-        $urlParams = $queryParams    = ['userId' => $user->getId(), 'opponentUserId' => $opponentUserId];
-        $query = $internalMessageRepository->createQueryBuilder('im')
+        $urlParams      = $queryParams  = ['userId' => $user->getId(), 'opponentUserId' => $opponentUserId];
+        $query          = $internalMessageRepository->createQueryBuilder('im')
                                            ->where('im.to = :userId')
                                            ->andWhere('im.from = :opponentUserId')
                                            ->setParameters($queryParams)
                                            ->orderBy('im.date', 'DESC')
                                            ->getQuery();
 
-        $pager = $paginator->paginate($query, $page, self::PAGE_LIMIT);
+        $pager      = $paginator->paginate($query, $page, self::PAGE_LIMIT);
         $pagination = new Pagination($pager, self::PAGE_LIMIT, $urlGenerator);
 
-        $view = $this->view([
+        $view       = $this->view([
             'internalMessages'  => $pager->getItems(),
             'totalResults'      => $pagination->totalResults,
             'totalPages'        => $pagination->totalPages,
