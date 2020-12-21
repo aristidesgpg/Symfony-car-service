336ab95b91b1f9d3249091d3573f4b153c1ef68d
Tyler Holcomb <tyler@tholcomb.com>
2020-12-20 23:15:25 -0500

    Improved file upload error messages

--- src/Controller/RepairOrderVideoController.php
+++ src/Controller/RepairOrderVideoController.php
@@ -83,7 +83,9 @@ class RepairOrderVideoController extends AbstractFOSRestController {
         }
         $file = $request->files->get('video');
         if (!$file instanceof UploadedFile) {
-            return new ValidationResponse(['video' => 'File not found']);
+            return new ValidationResponse(['video' => 'File upload failed']);
+        } elseif ($file->getError() !== UPLOAD_ERR_OK) {
+            return new ValidationResponse(['video' => $file->getErrorMessage()]);
         }
         $user = $this->getUser();
         if (!$user instanceof User || $user->getId() === null) {
