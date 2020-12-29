98f10c8f20a379dcd50cca68fa17233e92979f53
gokhanmelma <gokhanmelma@gmail.com>
2020-12-29 08:57:02 -0500

    Add EmailQueueHelper

---
+++ src/Service/EmailQueueHelper.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Service;
+
+use Doctrine\ORM\EntityManagerInterface;
+
+class EmailQueueHelper {
+    private const EMAIL_QUE_ENDPOINT = 'http://isre.us/api/add-to-email-que';
+
+    /** @var string */
+    private $accessToken;
+
+    /**
+     * ShortcodeHelper constructor.
+     *
+     * @param string $accessToken
+     */
+    public function __construct (string $accessToken) {
+        $this->accessToken = $accessToken;
+    }
+
+    /**
+     * @param $to
+     * @param $from
+     * @param $cc
+     * @param $subject
+     * @param $body
+     *
+     * @return bool|mixed|string
+     */
+    public function addToEmailQue ($to, $from, $subject, $body, $cc = null) {
+        $curl       = curl_init(self::EMAIL_QUE_ENDPOINT);
+        $postFields = [
+            'access_token' => $this->accessToken,
+            'to_email'     => $to,
+            'cc_emails'    => $cc,
+            'from_email'   => $from,
+            'subject'      => $subject,
+            'body'         => $body
+        ];
+        curl_setopt($curl, CURLOPT_TIMEOUT, 10);
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $postFields);
+        $response = curl_exec($curl);
+        curl_close($curl);
+        $response = json_decode($response);
+        return $response;
+    }
+}
