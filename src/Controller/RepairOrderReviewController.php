515703a3f639a583158839322059ed90b14c62c6
talentdev2020 <polaris.center@outlook.com>
2021-01-07 21:06:07 -0700

    Add Endpoint for Rating/Platform

--- src/Controller/RepairOrderReviewController.php
+++ src/Controller/RepairOrderReviewController.php
@@ -26,7 +26,7 @@ class RepairOrderReviewController extends AbstractFOSRestController {
      * @Rest\Put("/api/repair-order-view/{id}/view")
      *
      * @SWG\Tag(name="Repair Order View")
-     * @SWG\Put(description="Update a repairOrderView")
+     * @SWG\Put(description="Update a repairOrderView status with Viewed")
      *
      * @SWG\Response(
      *     response="200",
@@ -51,6 +51,48 @@ class RepairOrderReviewController extends AbstractFOSRestController {
         
         $repairOrderReview->setStatus('Viewd');
         $repairOrderReview->setDateViewed(new \DateTime());
+
+        $em->persist($repairOrderReview);
+        $em->flush();
+
+        $view = $this->view($repairOrderReview);
+        $view->getContext()->setGroups(RepairOrderReview::GROUPS);
+
+        return $this->handleView($view);
+    }
+
+    /**
+     * @Rest\Put("/api/repair-order-view/{id}/outcome")
+     *
+     * @SWG\Tag(name="Repair Order View")
+     * @SWG\Put(description="Update a repairOrderView rating and platform")
+     *
+     * @SWG\Parameter(name="rating", required=true, type="string", in="formData", enum={"poor", "average", "great"})
+     * @SWG\Parameter(name="platform", required=true, type="string", in="formData", enum={"facebook", "google"})
+     * 
+     * @SWG\Response(
+     *     response="200",
+     *     description="Success!",
+     *     @SWG\Schema(type="object", ref=@Model(type=RepairOrderReview::class, groups=RepairOrderReview::GROUPS))
+     * )
+     * @SWG\Response(response="404", description="ROR does not exist")
+     *
+     * @param RepairOrderReview      $repairOrderReview
+     * @param Request                $request
+     * @param EntityManagerInterface $em
+     *
+     * @return Response
+     */
+
+    public function outcome (RepairOrderReview $repairOrderReview, Request $request, EntityManagerInterface $em) {
+       
+        $rating   = $request->get('rating');
+        $platform = $request->get('platform');
+
+        $repairOrderReview->setStatus('Completed');
+        $repairOrderReview->setDateCompleted(new \DateTime());
+        $repairOrderReview->setRating($rating);
+        $repairOrderReview->setPlatform($platform);
 
         $em->persist($repairOrderReview);
         $em->flush();
