a219a049614495fd901d7d15a4b0d4d3573d5573
gokhanmelma <gokhanmelma@gmail.com>
2021-01-11 07:44:14 -0500

    Update data fixture

--- src/DataFixtures/RepairOrderInteractionFixture.php
+++ src/DataFixtures/RepairOrderInteractionFixture.php
@@ -22,13 +22,20 @@ class RepairOrderInteractionFixture extends Fixture implements DependentFixtureI
         $faker = Factory::create();
         for ($i = 1; $i <= 50; $i++) {
             $repairOrderInteraction = new RepairOrderInteraction();
-            $userReference          = $faker->numberBetween(1, 50);
-            $customerReference      = $faker->numberBetween(1, 50);
             $repairOrderReference   = $faker->numberBetween(1, 50);
+            $userReference          = $faker->numberBetween(1, 50);
             $interactionType        = ['waiver_signed', 'waiver_sent', 'waiver_viewed', 'waiver_acknowledged', 'waiver_re-sent'];
 
+            $ro = $manager->getRepository(RepairOrder::class)->findOneBy(['id' => $repairOrderReference]);
+            if ($ro) {
+                $customer           = $ro->getPrimaryCustomer();
+            }
+            else {
+                $customer           = $this->getReference('customer_' . $faker->numberBetween(1, 50));
+            }
+
             $repairOrderInteraction->setRepairOrder($this->getReference('repairOrder_' . $repairOrderReference))
-                                ->setCustomer($this->getReference('customer_' . $customerReference))
+                                ->setCustomer($customer)
                                 ->setUser($this->getReference('user_' . $userReference))
                                 ->setType($faker->randomElement($interactionType))
                                 ->setDate($faker->dateTimeBetween('-1 year'));
