decc43460fb137e93f57f4ab0d6742e7579257be
Brian Hurst <brian-hurst@outlook.com>
2021-01-12 15:05:40 -0700

    Added Symfony Mailer

--- src/Service/MailerHelper.php
+++ src/Service/MailerHelper.php
@@ -2,7 +2,11 @@
 
 namespace App\Service;
 
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 /**
  * Class MailerHelper.
@@ -24,7 +28,7 @@ class MailerHelper
      */
     private $sender;
 
-    public function __construct(ParameterBagInterface $parameterBag, \Swift_Mailer $mailer)
+    public function __construct(ParameterBagInterface $parameterBag, MailerInterface $mailer)
     {
         $this->parameterBag = $parameterBag;
         $this->sender = $this->parameterBag->get('sender');
@@ -33,15 +37,56 @@ class MailerHelper
 
     public function sendMail(string $title, string $email, string $body): bool
     {
-        $message = (new \Swift_Message($title))
-            ->setFrom($this->sender)
-            ->setTo($email)
-            ->setBody(
-                $body,
-                'text/plain'
-            );
+//        $message = (new \Swift_Message($title))
+//            ->setFrom($this->sender)
+//            ->setTo($email)
+//            ->setBody(
+//                $body,
+//                'text/plain'
+//            );
+//
+//        if (!$this->mailer->send($message)) {
+//            return false;
+//        }
 
-        if (!$this->mailer->send($message)) {
+        $email = (new Email())
+            ->from($this->sender)
+            ->to($email)
+            ->subject($title)
+            ->text($body);
+
+
+        return $this->send($email);
+    }
+
+
+    public function sendMailFromTemplate(string $title, string $email, string $templatePath, array $templateContext = []): bool
+    {
+
+        $email = (new TemplatedEmail())
+            ->from($this->sender)
+            ->to($email)
+            ->subject($title)
+            ->htmlTemplate($templatePath)
+            ->context($templateContext)
+        ;
+        return $this->send($email);
+
+    }
+
+
+
+    /**
+     * @param Email $email
+     * @return bool
+     */
+    public function send(Email $email): bool
+    {
+        try {
+            $this->mailer->send($email);
+        } catch (TransportExceptionInterface $e) {
+            //TODO Log error
+            dd($e->getMessage() );
             return false;
         }
 
