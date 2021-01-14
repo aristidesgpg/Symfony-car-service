2d09641edb0934e1d7784e72dcd8214117150756
talentdev2020 <polaris.center@outlook.com>
2021-01-14 13:50:05 -0700

    Add new/delete Endpoints

---
+++ src/Controller/RepairOrderCustomerController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\RepairOrder;
+use App\Entity\RepairOrderCustomer;
+use App\Repository\RepairOrderCustomerRepository;
+use App\Repository\CustomerRepository;
+use App\Response\ValidationResponse;
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use Swagger\Annotations as SWG;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Doctrine\ORM\EntityManagerInterface;
+
+/**
+ * Class RepairOrderCustomerController
+ *
+ * @package App\Controller
+ *
+ */
+class RepairOrderCustomerController extends AbstractFOSRestController {
+
+    /**
+     * @Rest\Post("/api/repair-order-customer/{id}")
+     *
+     * @SWG\Tag(name="RepairOrderCustomer")
+     * @SWG\Post(description="Add a Customer to RepairOrder")
+     *
+     * @SWG\Parameter(
+     *     name="customer",
+     *     type="integer",
+     *     description="Id of Customer",
+     *     in="formData",
+     *     required=true
+     * )
+     * 
+     * @SWG\Response(
+     *     response="200",
+     *     description="Success!",
+     *     @SWG\Schema(type="object", ref=@Model(type=RepairOrderCustomer::class, groups=RepairOrderCustomer::GROUPS))
+     * )
+     *
+     * @SWG\Response(
+     *     response="404",
+     *     description="Invalid Customer ID"
+     * )
+     *
+     * @param RepairOrder                $repairOrder
+     * @param Request                    $request
+     * @param CustomerRepository         $customerRepository
+     * @param EntityManagerInterface     $em
+     *
+     * @return Response
+     */
+
+    public function new (RepairOrder $repairOrder, Request $request, CustomerRepository $customerRepository, 
+                        EntityManagerInterface $em) {
+        $id = $request->get('customer');
+
+        $customer  = $customerRepository->findActive($id);
+        if(!$customer){
+            throw new NotFoundHttpException();
+        }
+
+        $repairOrderCustomer = new RepairOrderCustomer();
+
+        $repairOrderCustomer->setRepairOrder($repairOrder)
+                            ->setCustomer($customer);
+        
+        $em->persist($repairOrderCustomer);
+        $em->flush();
+
+        $view = $this->view($repairOrderCustomer);
+        $view->getContext()->setGroups($repairOrderCustomer::GROUPS);
+
+        return $this->handleView($view);
+    }
+
+    /**
+     * @Rest\Delete("/api/repair-order-customer/{id}")
+     *
+     * @SWG\Tag(name="RepairOrderCustomer")
+     * @SWG\Delete(description="Delete a customer")
+     * @SWG\Response(
+     *     response=200,
+     *     description="success",
+     *     @SWG\Items(
+     *         type="object",
+     *             @SWG\Property(property="status", type="string", description="status code", example={"status":
+     *                                              "Successfully deleted" }),
+     *         )
+     * )
+     *
+     * @param RepairOrderCustomer                 $repairOrderCustomer
+     * @param EntityManagerInterface $em
+     *
+     * @return Response
+     */
+    public function delete (RepairOrderCustomer $repairOrderCustomer, EntityManagerInterface $em) {
+ 
+        $em->remove($repairOrderCustomer);
+        $em->flush();
+
+        return $this->handleView($this->view('RepairOrderCustomer Deleted', Response::HTTP_OK));
+    }
+
+}
--- src/Entity/RepairOrderCustomer.php
+++ src/Entity/RepairOrderCustomer.php
@@ -4,29 +4,35 @@ namespace App\Entity;
 
 use App\Repository\RepairOrderCustomerRepository;
 use Doctrine\ORM\Mapping as ORM;
-
+use JMS\Serializer\Annotation as Serializer;
 /**
  * @ORM\Entity(repositoryClass=RepairOrderCustomerRepository::class)
  */
 class RepairOrderCustomer
 {
+    public const GROUPS = ['roc_list','ro_list', 'customer_list'];
     /**
      * @ORM\Id
      * @ORM\GeneratedValue
-     * @ORM\Column(type="integer")
+     * @ORM\Column(type="integer"),
+     * @Serializer\Groups(groups={"roc_list"})
      */
     private $id;
 
     /**
      * @ORM\ManyToOne(targetEntity=RepairOrder::class, inversedBy="repairOrderCustomers")
-     * @ORM\JoinColumn(nullable=false)
+     * @ORM\JoinColumn(nullable=false),
+     * @Serializer\Groups(groups={"roc_list"})
+     * 
      */
     private $repairOrder;
 
     /**
      * @ORM\ManyToOne(targetEntity=Customer::class, inversedBy="repairOrderCustomers")
-     * @ORM\JoinColumn(nullable=false)
+     * @ORM\JoinColumn(nullable=false),
+     * @Serializer\Groups(groups={"roc_list"})}
      */
+
     private $customer;
 
     public function getId(): ?int
