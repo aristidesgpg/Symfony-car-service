7897b4c9808afb35ab1de92496657d17dc621d45
PonyJackal <pony1125@outlook.com>
2021-01-18 11:23:55 -0500

    store pulic url in coupon image

--- src/DataFixtures/CouponFixture.php
+++ src/DataFixtures/CouponFixture.php
@@ -6,9 +6,7 @@ use App\Entity\Coupon;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
-use App\Service\ImageUploader;
 use Symfony\Component\DependencyInjection\ContainerInterface as Container;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 /**
  * Class CouponFixtures
@@ -18,11 +16,6 @@ use Symfony\Component\HttpFoundation\File\UploadedFile;
 class CouponFixture extends Fixture {
 
     /**
-     * @var ImageUploader
-     */
-    private $imageUploader;
-
-    /**
      * @var Container
      */
     private $container;
@@ -30,32 +23,10 @@ class CouponFixture extends Fixture {
     /**
      * CouponFixtures constructor.
      *
-     * @param ImageUploader $imageUploader
      * @param Container     $container
      */
-    public function __construct (ImageUploader $imageUploader, Container $container) {
-        $this->imageUploader = $imageUploader;
+    public function __construct (Container $container) {
         $this->container     = $container;
-    }
-
-    /**
-     * Create UploadedFile object from public url.
-     *
-     * @return UploadedFile|null
-     *
-     * @var array
-     */
-    public function createFileObject ($url) {
-        $rawData = file_get_contents($url);
-        $imgRaw  = imagecreatefromstring($rawData);
-
-        if ($imgRaw !== false) {
-            imagejpeg($imgRaw, $this->container->getParameter('uploads_directory') . 'tmp.jpg', 100);
-            imagedestroy($imgRaw);
-
-            return new UploadedFile($this->container->getParameter('uploads_directory') . 'tmp.jpg', 'tmp.jpg', 'image/jpeg', null, null, true);
-        }
-        return null;
     }
 
     /**
@@ -68,20 +39,8 @@ class CouponFixture extends Fixture {
         // Load some coupons
         for ($i = 1; $i <= 10; $i++) {
             $coupon = new Coupon();
-            //upload a random image
-            $file = $this->createFileObject($image);
-            $path = '';
-
-            if ($file) {
-                $path = $this->imageUploader->uploadImage($file, 'coupons');
-            }
-
-            if (!$path) {
-                continue;
-            }
-
             $coupon->setTitle($faker->sentence($nbWords = 3, $variableNbWords = true))
-                   ->setImage($path)
+                   ->setImage($image)
                    ->setDeleted($faker->boolean(30));
 
             $manager->persist($coupon);
