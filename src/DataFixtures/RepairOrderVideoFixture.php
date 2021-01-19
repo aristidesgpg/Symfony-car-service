5aad86834f438eaf8c51c0b4462d0ee8a930dc86
PonyJackal <pony1125@outlook.com>
2021-01-19 11:58:42 -0500

    Update RepairOrderVideo dataFixture

--- src/DataFixtures/RepairOrderVideoFixture.php
+++ src/DataFixtures/RepairOrderVideoFixture.php
@@ -19,20 +19,65 @@ class RepairOrderVideoFixture extends Fixture implements DependentFixtureInterfa
     public function load (ObjectManager $manager) {
         $faker = Factory::create();
         for ($i = 1; $i < 51; $i++) {
-            /** @var RepairOrder $ro */
-            $ro      = $this->getReference("repairOrder_{$i}");
-            $created = $faker->dateTimeThisYear;
-            $video   = new RepairOrderVideo();
-            $video->setRepairOrder($ro)
-                  ->setTechnician($ro->getPrimaryTechnician())
-                  ->setDateCreated($created)
-                  ->setPath($faker->randomElement(self::VIDEO_FIXTURES));
-            $interaction = new RepairOrderVideoInteraction();
-            $interaction->setRepairOrderVideo($video)
-                        ->setType('Created')
-                        ->setDate($created);
-            $video->addInteraction($interaction);
-            $manager->persist($video);
+            /** @var RepairOrder $repairOrder */
+            $repairOrder  = $this->getReference("repairOrder_{$i}");
+            $dateUploaded = $faker->dateTimeThisYear;
+            $status       = "Uploaded";
+            //create repairOrderVideo
+            $repairOrderVideo = new RepairOrderVideo();
+            $repairOrderVideo->setRepairOrder($repairOrder)
+                              ->setTechnician($repairOrder->getPrimaryTechnician())
+                              ->setDateUploaded($dateUploaded)
+                              ->setPath($faker->randomElement(self::VIDEO_FIXTURES));
+            //create repairOrderVideoInteraction
+            $repairOrderMPIInteraction = new RepairOrderVideoInteraction();
+            $repairOrderMPIInteraction->setRepairOrderVideo($repairOrderVideo)
+                                      ->setUser($repairOrder->getPrimaryTechnician())
+                                      ->setCustomer($repairOrder->getPrimaryCustomer())
+                                      ->setType($status)
+                                      ->setDate($dateUploaded);
+            $repairOrderVideo->addInteraction($repairOrderMPIInteraction);
+            //Sent
+            if($faker->boolean(50)){
+                $dateSent       = clone $dateUploaded;
+                $dateSentModify = random_int(1, 12);
+                $dateSent       = $dateSent->modify('+' . $dateSentModify . ' hours');
+                $status         = "Sent";
+                //update repairOrderVideo sent date
+                $repairOrderVideo->setDateSent($dateSent);
+                //create repairOrderVideoInteraction
+                $repairOrderMPIInteraction = new RepairOrderVideoInteraction();
+                $repairOrderMPIInteraction->setRepairOrderVideo($repairOrderVideo)
+                                          ->setUser($repairOrder->getPrimaryTechnician())
+                                          ->setCustomer($repairOrder->getPrimaryCustomer())
+                                          ->setType($status)
+                                          ->setDate($dateUploaded);
+                $repairOrderVideo->addInteraction($repairOrderMPIInteraction);
+                //Viewed
+                if($faker->boolean(50)){
+                    $dateViewed       = clone $dateSent;
+                    $dateViewedModify = random_int(1, 12);
+                    $dateViewed       = $dateViewed->modify('+' . $dateViewedModify . ' hours');
+                    $status           = "Viewed";
+                    //update repairOrderVideo viewed date
+                    $repairOrderVideo->setDateViewed($dateSent);
+                    //create repairOrderVideoInteraction
+                    $repairOrderMPIInteraction = new RepairOrderVideoInteraction();
+                    $repairOrderMPIInteraction->setRepairOrderVideo($repairOrderVideo)
+                                              ->setUser($repairOrder->getPrimaryTechnician())
+                                              ->setCustomer($repairOrder->getPrimaryCustomer())
+                                              ->setType($status)
+                                              ->setDate($dateViewed);
+                    $repairOrderVideo->addInteraction($repairOrderMPIInteraction);
+                }
+            }
+
+            //update repairOrderVideo status
+            $repairOrderVideo->setStatus($status);
+            $manager->persist($repairOrderVideo);
+            //update repairOrder video status
+            $repairOrder->setVideoStatus($status);
+            $manager->persist($repairOrder);
         }
         $manager->flush();
     }
