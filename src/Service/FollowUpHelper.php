ee65564d0fc5e3be5f001bdcc64d117b219566da
talentdev2020 <polaris.center@outlook.com>
2021-01-19 03:57:25 -0700

    Create newFollowupInteraction

--- src/Service/FollowUpHelper.php
+++ src/Service/FollowUpHelper.php
@@ -3,7 +3,9 @@
 namespace App\Service;
 
 use App\Entity\FollowUp;
+use App\Entity\FollowUpInteraction;
 use App\Entity\User;
+use App\Entity\Customer;
 use App\Entity\RepairOrder;
 use Doctrine\ORM\EntityManagerInterface;
 use Exception;
@@ -38,18 +40,34 @@ class CheckInHelper {
                 ->setDateCreated( new \DateTime )
                 ->setStatus('Sent');
 
-       $em->persist($followup);
-       $em->flush();
+       $this->em->persist($followup);
+       $this->em->flush();
 
        $this->sendMessage();
 
     }
     
     /**
-     * @param array $params
+     * @param FollowUp $followup
+     * @param User/Customer     $user
+     * @param string $status
+     * 
      */
-    public function createFollowUpInteraction (): void {
-       
+    public function createFollowUpInteraction (FollowUp $followup, $user, string $status): void {
+       $followupInteraction = new FollowUpInteraction();
+
+       $followupInteraction->setFollowUp($followup)
+                           ->setStatus($status);
+        if($status === 'Sent'){
+            $followupInteraction->setUser($user);
+        }
+        else {
+            $followupInteraction->setCustomer($user);
+        }
+
+        $this->em->persist($followup);
+        $this->em->flush();
+ 
     }
     
     /**
