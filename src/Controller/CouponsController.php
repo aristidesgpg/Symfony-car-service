14f070bf4add61f35cff82a32305cb07e6cf67aa
Laramie Rugen <ffgpga08@gmail.com>
2021-01-20 10:28:11 -0500

    Refactoring & Styling review of coupons pagination

--- src/Controller/CouponsController.php
+++ src/Controller/CouponsController.php
@@ -2,57 +2,54 @@
 
 namespace App\Controller;
 
+use App\Entity\Coupon;
 use App\Repository\CouponRepository;
+use App\Response\ValidationResponse;
 use App\Service\ImageUploader;
+use App\Service\Pagination;
 use Doctrine\ORM\EntityManagerInterface;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
-use Swagger\Annotations as SWG;
+use Knp\Component\Pager\PaginatorInterface;
 use Nelmio\ApiDocBundle\Annotation\Model;
+use Swagger\Annotations as SWG;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
-use App\Entity\Coupon;
-use App\Service\Pagination;
-use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
-use App\Response\ValidationResponse;
 
-/**
- * Class CouponsController
- *
- * @package App\Controller
- */
-class CouponsController extends AbstractFOSRestController {
+class CouponsController extends AbstractFOSRestController
+{
     private const PAGE_LIMIT = 100;
+
     /**
      * @Rest\Get("/api/coupons")
      *
      * @SWG\Tag(name="Coupons")
      * @SWG\Get(description="Get all coupons")
-     * 
+     *
      * @SWG\Parameter(name="page", type="integer", in="query")
-     * 
      * @SWG\Parameter(
      *     name="pageLimit",
      *     type="integer",
      *     description="Page Limit",
      *     in="query"
      * )
-     *  @SWG\Parameter(
+     * @SWG\Parameter(
      *     name="sortField",
      *     type="string",
      *     description="The name of sort field",
      *     in="query"
      * )
-     *  @SWG\Parameter(
+     * @SWG\Parameter(
      *     name="sortDirection",
      *     type="string",
      *     description="The direction of sort",
      *     in="query",
      *     enum={"ASC", "DESC"}
      * )
-     *  @SWG\Parameter(
+     * @SWG\Parameter(
      *     name="searchField",
      *     type="string",
      *     description="The name of search field",
@@ -81,52 +78,50 @@ class CouponsController extends AbstractFOSRestController {
      *
      * @SWG\Response(response="404", description="Invalid page parameter")
      * @SWG\Response(response="406", ref="#/responses/ValidationResponse")
-     * 
-     * @param Request                $request
-     * @param EntityManagerInterface $em
-     * @param CouponRepository       $couponRepository
      *
-     * @param PaginatorInterface    $paginator
-     * @param UrlGeneratorInterface $urlGenerator
      * @return Response
      */
-    public function list (Request $request,EntityManagerInterface $em, CouponRepository $couponRepository,PaginatorInterface $paginator,
-    UrlGeneratorInterface $urlGenerator) {
-        $page            = $request->query->getInt('page', 1);
-        $urlParameters   = [];
+    public function list(
+        Request $request,
+        EntityManagerInterface $em,
+        CouponRepository $couponRepository,
+        PaginatorInterface $paginator,
+        UrlGeneratorInterface $urlGenerator
+    ) {
+        $page = $request->query->getInt('page', 1);
+        $urlParameters = [];
         $queryParameters = [];
-        $errors          = [];
-        $sortField       = "";
-        $sortDirection   = "";
-        $searchField     = "";
-        $searchTerm      = "";
+        $errors = [];
+        $sortField = '';
+        $sortDirection = '';
+        $searchField = '';
+        $searchTerm = '';
+        $columns = $em->getClassMetadata('App\Entity\Coupon')->getFieldNames();
 
         if ($page < 1) {
             throw new NotFoundHttpException();
         }
 
-        $columns                        = $em->getClassMetadata('App\Entity\Coupon')->getFieldNames();
+        if ($request->query->has('sortField') && $request->query->has('sortDirection')) {
+            $sortField = $request->query->get('sortField');
 
-        if($request->query->has('sortField') && $request->query->has('sortDirection'))
-        {
-            $sortField                  = $request->query->get('sortField');
-            
-            //check if the sortfield exist
-            if(!in_array($sortField, $columns))
-                $errors['sortField']   = 'Invalid sort field name';
-            
-            $sortDirection             = $request->query->get('sortDirection');
+            // check if the sortField exist
+            if (!in_array($sortField, $columns)) {
+                $errors['sortField'] = 'Invalid sort field name';
+            }
+
+            $sortDirection = $request->query->get('sortDirection');
         }
 
-        if($request->query->has('searchField') && $request->query->has('searchTerm'))
-        {
-            $searchField               = $request->query->get('searchField');
-           
-            //check if the searchfield exist
-            if(!in_array($searchField, $columns))
-                $errors['searchField'] = 'Invalid search field name';
+        if ($request->query->has('searchField') && $request->query->has('searchTerm')) {
+            $searchField = $request->query->get('searchField');
 
-            $searchTerm  = $request->query->get('searchTerm');
+            //check if the searchField exist
+            if (!in_array($searchField, $columns)) {
+                $errors['searchField'] = 'Invalid search field name';
+            }
+
+            $searchTerm = $request->query->get('searchTerm');
         }
 
         if (!empty($errors)) {
@@ -136,40 +131,40 @@ class CouponsController extends AbstractFOSRestController {
         $qb = $couponRepository->createQueryBuilder('co');
         $qb->andWhere('co.deleted = 0');
 
-        if($searchTerm)
-        {
+        if ($searchTerm) {
             $qb->andWhere('co.'.$searchField.' LIKE :searchTerm');
-            $queryParameters['searchTerm']  = '%'.$searchTerm.'%';
-            
-            $urlParameters['searchField']   = $searchField;
-            $urlParameters['searchTerm']    = $searchTerm;
+            $queryParameters['searchTerm'] = '%'.$searchTerm.'%';
+
+            $urlParameters['searchField'] = $searchField;
+            $urlParameters['searchTerm'] = $searchTerm;
         }
-       
-        if($sortDirection)
-        {
+
+        if ($sortDirection) {
             $qb->orderBy('co.'.$sortField, $sortDirection);
 
-            $urlParameters['sortField']     = $sortField;
+            $urlParameters['sortField'] = $sortField;
             $urlParameters['sortDirection'] = $sortDirection;
         }
 
         $q = $qb->getQuery();
         $q->setParameters($queryParameters);
 
-        $pageLimit      = $request->query->getInt('pageLimit', self::PAGE_LIMIT);
+        $pageLimit = $request->query->getInt('pageLimit', self::PAGE_LIMIT);
 
-        $pager          = $paginator->paginate($q, $page, $pageLimit);
-        $pagination     = new Pagination($pager, $pageLimit, $urlGenerator);
+        $pager = $paginator->paginate($q, $page, $pageLimit);
+        $pagination = new Pagination($pager, $pageLimit, $urlGenerator);
 
-        $view = $this->view([
-            'coupons'      => $pager->getItems(),
-            'totalResults' => $pagination->totalResults,
-            'totalPages'   => $pagination->totalPages,
-            'previous'     => $pagination->getPreviousPageURL('app_coupons_list', $urlParameters),
-            'currentPage'  => $pagination->currentPage,
-            'next'         => $pagination->getNextPageURL('app_coupons_list', $urlParameters)
-        ]);
-        
+        $view = $this->view(
+            [
+                'coupons'      => $pager->getItems(),
+                'totalResults' => $pagination->totalResults,
+                'totalPages'   => $pagination->totalPages,
+                'previous'     => $pagination->getPreviousPageURL('app_coupons_list', $urlParameters),
+                'currentPage'  => $pagination->currentPage,
+                'next'         => $pagination->getNextPageURL('app_coupons_list', $urlParameters),
+            ]
+        );
+
         $view->getContext()->setGroups(['coupon_list']);
 
         return $this->handleView($view);
@@ -212,7 +207,8 @@ class CouponsController extends AbstractFOSRestController {
      *
      * @return Response
      */
-    public function new (EntityManagerInterface $em, Request $request, ImageUploader $imageUploader) {
+    public function new(EntityManagerInterface $em, Request $request, ImageUploader $imageUploader)
+    {
         $title = $request->get('title');
         $image = $request->files->get('image');
 
@@ -223,7 +219,9 @@ class CouponsController extends AbstractFOSRestController {
 
         $path = $imageUploader->uploadImage($image, 'coupons');
         if (!$path) {
-            return $this->handleView($this->view('Something Went Wrong Trying to Upload the Image', Response::HTTP_INTERNAL_SERVER_ERROR));
+            return $this->handleView(
+                $this->view('Something Went Wrong Trying to Upload the Image', Response::HTTP_INTERNAL_SERVER_ERROR)
+            );
         }
 
         $coupon = new Coupon();
@@ -264,8 +262,12 @@ class CouponsController extends AbstractFOSRestController {
      *
      * @return Response
      */
-    public function edit (Coupon $coupon, Request $request, EntityManagerInterface $em,
-                          ImageUploader $imageUploader) {
+    public function edit(
+        Coupon $coupon,
+        Request $request,
+        EntityManagerInterface $em,
+        ImageUploader $imageUploader
+    ) {
         $title = $request->get('title');
 
         // Validation
@@ -274,7 +276,7 @@ class CouponsController extends AbstractFOSRestController {
         }
 
         $coupon->setTitle($title);
-        
+
         $em->persist($coupon);
         $em->flush();
 
@@ -301,7 +303,8 @@ class CouponsController extends AbstractFOSRestController {
      *
      * @return Response
      */
-    public function delete (Coupon $coupon, EntityManagerInterface $em) {
+    public function delete(Coupon $coupon, EntityManagerInterface $em)
+    {
         $coupon->setDeleted(true);
 
         $em->persist($coupon);
