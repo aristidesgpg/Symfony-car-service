4d40ab64f723518e258acb98f29e3c03a2126652
PonyJackal <pony1125@outlook.com>
2021-01-25 14:28:25 -0500

    update dataFixture for serviceSMS

--- src/DataFixtures/ServiceSMSFixture.php
+++ src/DataFixtures/ServiceSMSFixture.php
@@ -19,12 +19,15 @@ class ServiceSMSFixture extends Fixture implements DependentFixtureInterface {
      */
     public function load (ObjectManager $manager) {
         $faker = Factory::create();
+        $messageStatus = ["queued", "failed", "sent", "delivered", "undelivered"];
         
-        for($i = 0; $i < 50; $i ++){
+        for($i = 0; $i < 500; $i ++){
             $userReference     = $faker->numberBetween(1, 49);
             $customerReference = $faker->numberBetween(1, 49);
             $user              = $this->getReference('user_' . $userReference);
             $customer          = $this->getReference('customer_' . $customerReference);
+            $status            = $faker->randomElement($messageStatus);
+            $date              = $faker->dateTimeBetween('-1 year');
            
             $serviceSMS = new ServiceSMS();
             $serviceSMS->setUser($user)
@@ -32,7 +35,9 @@ class ServiceSMSFixture extends Fixture implements DependentFixtureInterface {
                        ->setPhone($customer->getPhone())
                        ->setMessage($faker->sentence($nbWords = 5, $variableNbWords = true))
                        ->setIncoming($faker->boolean(60))
-                       ->setIsRead($faker->boolean(80));
+                       ->setIsRead($faker->boolean(80))
+                       ->setStatus($status)
+                       ->setDate($date);
                        
             $manager->persist($serviceSMS);
             $manager->flush();
