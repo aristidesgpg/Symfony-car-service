f69c2894d97a96ee262fc0986309c7f5d8f963b9
Laramie Rugen <ffgpga08@gmail.com>
2021-01-26 22:31:19 -0500

    fixed a bug when logged in as tech not being able to get ROs

--- src/Repository/RepairOrderRepository.php
+++ src/Repository/RepairOrderRepository.php
@@ -4,10 +4,10 @@ namespace App\Repository;
 
 use App\Entity\RepairOrder;
 use App\Entity\User;
+use DateTime;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\ORM\NonUniqueResultException;
 use Doctrine\Persistence\ManagerRegistry;
-use DateTime;
 use Exception;
 
 /**
@@ -55,86 +55,91 @@ class RepairOrderRepository extends ServiceEntityRepository
 
             foreach ($fields as $name => $value) {
                 if ($value !== null) {
-                    if ($name === "open") {
-                        if ($value)
-                            $qb->andWhere('ro.dateClosed IS NULL');
-                        else
-                            $qb->andWhere('ro.dateClosed IS NOT NULL');
-                    } else if ($name === 'needsVideo') {
+                    if ($name === 'open') {
                         if ($value) {
-                            $qb->andWhere('ro.videoStatus = :videoStatus');
-                            $qb->setParameter("videoStatus", 'Not Started');
+                            $qb->andWhere('ro.dateClosed IS NULL');
+                        } else {
+                            $qb->andWhere('ro.dateClosed IS NOT NULL');
                         }
                     } else {
-                        $qb->andWhere("ro.$name = :$name");
-                        $qb->setParameter($name, $value);
+                        if ($name === 'needsVideo') {
+                            if ($value) {
+                                $qb->andWhere('ro.videoStatus = :videoStatus')
+                                   ->setParameter('videoStatus', 'Not Started');
+                            }
+                        } else {
+                            $qb->andWhere("ro.$name = :$name")
+                               ->setParameter($name, $value);
+                        }
                     }
                 }
             }
 
             if ($startDate && $endDate) {
                 try {
-                    $startDate     = new DateTime($startDate);
-                    $endDate       = new DateTime($endDate);
+                    $startDate = new DateTime($startDate);
+                    $endDate = new DateTime($endDate);
 
                     $qb->andWhere('ro.dateCreated BETWEEN :startDate AND :endDate')
-                        ->setParameter('startDate', $startDate)
-                        ->setParameter('endDate', $endDate);
+                       ->setParameter('startDate', $startDate)
+                       ->setParameter('endDate', $endDate);
                 } catch (Exception $e) {
                     $errors['date'] = 'Invalid date format';
                 }
             }
 
             if ($searchTerm) {
-                $query              = "";
-                $qb->leftJoin('ro.primaryCustomer', 'ro_customer');
-                $qb->leftJoin('ro.primaryTechnician', 'ro_technician');
-                $qb->leftJoin('ro.primaryAdvisor', 'ro_advisor');
+                $query = '';
+                $qb->leftJoin('ro.primaryCustomer', 'ro_customer')
+                   ->leftJoin('ro.primaryTechnician', 'ro_technician')
+                   ->leftJoin('ro.primaryAdvisor', 'ro_advisor');
 
                 $searchFields = [
-                    "ro"             => ["number", "year", "model", "miles", "vin"],
-                    "ro_customer"    => ["name", "phone", "email"],
-                    "ro_advisor"     => ["combine_name", "phone", "email"],
-                    "ro_technician"  => ["combine_name", "phone", "email"],
-
-
+                    'ro' => ['number', 'year', 'model', 'miles', 'vin'],
+                    'ro_customer' => ['name', 'phone', 'email'],
+                    'ro_advisor' => ['combine_name', 'phone', 'email'],
+                    'ro_technician' => ['combine_name', 'phone', 'email'],
                 ];
 
                 foreach ($searchFields as $class => $fields) {
                     foreach ($fields as $field) {
-                        if ($field === "combine_name")
+                        if ($field === 'combine_name') {
                             $query .= "CONCAT($class.firstName , ' ' , $class.lastName) LIKE :searchTerm OR ";
-                        else
+                        } else {
                             $query .= "$class.$field LIKE :searchTerm OR ";
+                        }
                     }
                 }
 
                 $query = substr($query, 0, strlen($query) - 4);
 
                 $qb->andWhere($query)
-                    ->setParameter('searchTerm', '%' . $searchTerm . '%');
+                   ->setParameter('searchTerm', '%'.$searchTerm.'%');
             }
 
 
             if ($user instanceof User) {
                 if (in_array('ROLE_SERVICE_ADVISOR', $user->getRoles())) {
                     if ($user->getShareRepairOrders()) {
-                        $qb->andWhere('ro.primaryAdvisor IN (:users)');
+                        $qb->andWhere('ro.primaryAdvisor IN (:users)')
+                           ->setParameter('user', $user);
                         $queryParameters['users'] = $userRepo->getSharedUsers();
                     } else {
-                        $qb->andWhere('ro.primaryAdvisor = :user');
+                        $qb->andWhere('ro.primaryAdvisor = :user')
+                           ->setParameter('user', $user);
                         $queryParameters['user'] = $user;
                     }
                 } elseif (in_array('ROLE_TECHNICIAN', $user->getRoles())) {
-                    $qb->andWhere('ro.primaryTechnician = :user');
+                    $qb->andWhere('ro.primaryTechnician = :user')
+                       ->setParameter('user', $user);
                     $queryParameters['user'] = $user;
                 }
             }
 
             if ($sortDirection) {
-                $qb->orderBy('ro.' . $sortField, $sortDirection);
+                $qb->orderBy('ro.'.$sortField, $sortDirection);
 
-                $urlParameters['sortField']     = $sortField;
+                $urlParameters['sortField'] = $sortField;
                 $urlParameters['sortDirection'] = $sortDirection;
             }
 
@@ -144,11 +149,13 @@ class RepairOrderRepository extends ServiceEntityRepository
                     $item->setRepairOrderQuote(null);
                 }
             }
+
             return $items;
         } catch (NonUniqueResultException $e) {
             return null;
         }
     }
+
     /**
      * @param string $uid
      *
@@ -163,13 +170,13 @@ class RepairOrderRepository extends ServiceEntityRepository
             }
 
             return $this->createQueryBuilder('r')
-                ->orWhere('r.number= :uid')
-                ->orWhere('r.linkHash = :uid')
-                ->setParameter('uid', $uid)
-                ->orderBy('r.id', 'DESC')
-                ->setMaxResults(1)
-                ->getQuery()
-                ->getOneOrNullResult();
+                        ->orWhere('r.number= :uid')
+                        ->orWhere('r.linkHash = :uid')
+                        ->setParameter('uid', $uid)
+                        ->orderBy('r.id', 'DESC')
+                        ->setMaxResults(1)
+                        ->getQuery()
+                        ->getOneOrNullResult();
         } catch (NonUniqueResultException $e) {
             // shouldn't happen
             return null;
@@ -185,10 +192,10 @@ class RepairOrderRepository extends ServiceEntityRepository
     {
         try {
             return $this->createQueryBuilder('ro')
-                ->andWhere('ro.number = :number')
-                ->setParameter('number', $number)
-                ->getQuery()
-                ->getOneOrNullResult();
+                        ->andWhere('ro.number = :number')
+                        ->setParameter('number', $number)
+                        ->getQuery()
+                        ->getOneOrNullResult();
         } catch (NonUniqueResultException $e) {
             return null;
         }
@@ -203,10 +210,10 @@ class RepairOrderRepository extends ServiceEntityRepository
     {
         try {
             return $this->createQueryBuilder('ro')
-                ->andWhere('ro.linkHash = :hash')
-                ->setParameter('hash', $linkHash)
-                ->getQuery()
-                ->getOneOrNullResult();
+                        ->andWhere('ro.linkHash = :hash')
+                        ->setParameter('hash', $linkHash)
+                        ->getQuery()
+                        ->getOneOrNullResult();
         } catch (NonUniqueResultException $e) {
             return null;
         }
