6fdfeacba339bda44a3b5eff4bd4fd8e800d6cfb
talentdev2020 <polaris.center@outlook.com>
2021-01-27 17:39:56 -0700

    Modify response

--- src/Controller/MyReviewController.php
+++ src/Controller/MyReviewController.php
@@ -15,6 +15,7 @@ use Nelmio\ApiDocBundle\Annotation\Model;
 use Swagger\Annotations as SWG;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotAcceptableHttpException;
 use Doctrine\ORM\EntityManagerInterface;
 use DateTime;
 
@@ -24,8 +25,9 @@ use DateTime;
  * @package App\Controller
  *
  */
-class MyReviewController extends AbstractFOSRestController {
-   
+class MyReviewController extends AbstractFOSRestController
+{
+
     /**
      * @Rest\Post("/api/my-review/{id}/view")
      *
@@ -33,32 +35,33 @@ class MyReviewController extends AbstractFOSRestController {
      * @SWG\Post(description="Id of RepairOrderReview.  Update a repairOrderView and Create a new reviewInteraction")
      *
      * @SWG\Response(response="200", description="Success!")
-     *
+     * @SWG\Response(response="406", description="Invalid parameter")
      * @param RepairOrderReview                       $repairOrderReview
      * @param EntityManagerInterface                  $em
      * @param RepairOrderReviewInteractionsRepository $reviewInteractions
      *
      * @return Response
      */
-    public function viewed (RepairOrderReview $repairOrderReview, EntityManagerInterface $em, RepairOrderReviewInteractionsRepository $reviewInteractions) {
+    public function viewed(RepairOrderReview $repairOrderReview, EntityManagerInterface $em, RepairOrderReviewInteractionsRepository $reviewInteractions)
+    {
         $user = $this->getUser();
 
-        if(!$user instanceof Customer){
-            return $this->handleView($this->view('The type of user should be Customer.', Response::HTTP_BAD_REQUEST));
+        if (!$user instanceof Customer) {
+            throw new NotAcceptableHttpException('The type of user should be Customer.');
         }
 
-        if(!$repairOrderReview){
-            return $this->handleView($this->view('The review of this repairOrder was not created yet.', Response::HTTP_BAD_REQUEST));
+        if (!$repairOrderReview) {
+            throw new NotAcceptableHttpException('The review of this repairOrder was not created yet.');
         }
 
-        if($repairOrderReview->getStatus() !=='Completed') {
+        if ($repairOrderReview->getStatus() !== 'Completed') {
             $repairOrderReview->setStatus('Viewd');
             $repairOrderReview->setDateViewed(new \DateTime());
-    
+
             $em->persist($repairOrderReview);
             $em->flush();
         }
-        
+
         $reviewInteractions->new($repairOrderReview, 'Viewed', $user);
 
         return $this->handleView($this->view(['message' => 'Success'], Response::HTTP_OK));
@@ -74,6 +77,7 @@ class MyReviewController extends AbstractFOSRestController {
      * @SWG\Parameter(name="platform", required=true, type="string", in="formData", enum={"facebook", "google"})
      * 
      * @SWG\Response(response="200", description="Success!")
+     * @SWG\Response(response="406", description="Invalid parameter")
      *
      * @param RepairOrderReview                       $repairOrderReview
      * @param Request                                 $request
@@ -82,19 +86,20 @@ class MyReviewController extends AbstractFOSRestController {
      *
      * @return Response
      */
-    public function outcome (RepairOrderReview $repairOrderReview, Request $request, EntityManagerInterface $em, RepairOrderReviewInteractionsRepository $reviewInteractions) {
+    public function outcome(RepairOrderReview $repairOrderReview, Request $request, EntityManagerInterface $em, RepairOrderReviewInteractionsRepository $reviewInteractions)
+    {
         $user     = $this->getUser();
 
-        if(!$user instanceof Customer){
-            return $this->handleView($this->view('The type of user should be Customer.', Response::HTTP_BAD_REQUEST));
+        if (!$user instanceof Customer) {
+            throw new NotAcceptableHttpException('The type of user should be Customer.');
         }
 
-        if(!$repairOrderReview){
-            return $this->handleView($this->view('The review of this repairOrder was not created yet.', Response::HTTP_BAD_REQUEST));
+        if (!$repairOrderReview) {
+            throw new NotAcceptableHttpException('The review of this repairOrder was not created yet.');
         }
 
-        if($repairOrderReview->getStatus() === 'Completed'){
-            return $this->handleView($this->view('This review was already completed.', Response::HTTP_BAD_REQUEST));
+        if ($repairOrderReview->getStatus() === 'Completed') {
+            throw new NotAcceptableHttpException('This review was already completed.');
         }
 
         $rating            = $request->get('rating');
@@ -120,32 +125,34 @@ class MyReviewController extends AbstractFOSRestController {
      * @SWG\Post(description="Resend a message to the customer and create a new interaction")
      *
      * @SWG\Response(response="200", description="Success!")
+     * @SWG\Response(response="406", description="Invalid parameter")
      *
      * @param RepairOrderReview                       $repairOrderReview
-     * @param EntityManagerInterface                  $em
      * @param RepairOrderReviewInteractionsRepository $reviewInteractions
      * @param MyReviewHelper                          $myReviewHelper
      *
      * @return Response
      */
-    public function resendMessage (RepairOrderReview $repairOrderReview, EntityManagerInterface $em, 
-                                   RepairOrderReviewInteractionsRepository $reviewInteractions, MyReviewHelper $myReviewHelper) {
+    public function resendMessage(
+        RepairOrderReview $repairOrderReview,
+        RepairOrderReviewInteractionsRepository $reviewInteractions,
+        MyReviewHelper $myReviewHelper
+    ) {
         $user = $this->getUser();
 
-        if(!$user instanceof User){
-            return $this->handleView($this->view('The type of user should be User', Response::HTTP_BAD_REQUEST));
+        if (!$user instanceof Customer) {
+            throw new NotAcceptableHttpException('The type of user should be Customer.');
         }
 
-        if(!$repairOrderReview){
-            return $this->handleView($this->view('The review of this repairOrder was not created yet.', Response::HTTP_BAD_REQUEST));
+        if (!$repairOrderReview) {
+            throw new NotAcceptableHttpException('The review of this repairOrder was not created yet.');
         }
 
         $reviewInteractions->new($repairOrderReview, 'Sent', $user);
-        
+
         $repairOrder = $repairOrderReview->getRepairOrder();
-        $myReviewHelper->sendMessage( $repairOrder );
+        $myReviewHelper->sendMessage($repairOrder);
 
         return $this->handleView($this->view(['message' => 'Success'], Response::HTTP_OK));
     }
-
 }
