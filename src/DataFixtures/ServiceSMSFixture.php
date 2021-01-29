07d2680205bb8f69536ed8a5d30d295c71857aed
Laramie Rugen <ffgpga08@gmail.com>
2021-01-29 10:35:18 -0500

    Fix to users being loaded onto incoming services messages in the fixture

--- src/DataFixtures/ServiceSMSFixture.php
+++ src/DataFixtures/ServiceSMSFixture.php
@@ -8,56 +8,51 @@ use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
 
-/**
- * Class ServiceSMSFixture
- *
- * @package App\DataFixtures
- */
-class ServiceSMSFixture extends Fixture implements DependentFixtureInterface {
-    /**
-     * @param ObjectManager $manager
-     */
-    public function load (ObjectManager $manager) {
+class ServiceSMSFixture extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
         $faker = Factory::create();
-        $messageStatus = ["queued", "failed", "sent", "delivered", "undelivered"];
-        
-        for($i = 0; $i < 500; $i ++){
-            $userReference     = $faker->numberBetween(1, 49);
+        $messageStatus = ['queued', 'failed', 'sent', 'delivered', 'undelivered'];
+
+        for ($i = 0; $i < 500; $i++) {
+            $userReference = $faker->numberBetween(1, 49);
             $customerReference = $faker->numberBetween(1, 49);
-            $user              = $this->getReference('user_' . $userReference);
-            $customer          = $this->getReference('customer_' . $customerReference);
-            $status            = $faker->randomElement($messageStatus);
-            $date              = $faker->dateTimeBetween('-1 year');
-            $inComing          = $faker->boolean(60);
-           
+            $user = $this->getReference('user_'.$userReference);
+            $customer = $this->getReference('customer_'.$customerReference);
+            $status = $faker->randomElement($messageStatus);
+            $date = $faker->dateTimeBetween('-1 year');
+            $incoming = $faker->boolean(60);
+
             $serviceSMS = new ServiceSMS();
-            $serviceSMS->setUser($user)
-                       ->setCustomer($customer)
+            $serviceSMS->setCustomer($customer)
                        ->setPhone($customer->getPhone())
                        ->setMessage($faker->sentence($nbWords = 5, $variableNbWords = true))
                        ->setIsRead($faker->boolean(80))
-                       ->setIncoming($inComing)
+                       ->setIncoming($incoming)
                        ->setDate($date);
-            
-            if(!$inComing){
+
+            if (!$incoming) {
                 $serviceSMS->setStatus($status)
-                           ->setIsRead(true);
+                           ->setIsRead(true)
+                           ->setUser($user);
             }
-                       
+
             $manager->persist($serviceSMS);
             $manager->flush();
 
-            $this->addReference('serviceSMS_' . $i, $serviceSMS);
+            $this->addReference('serviceSMS_'.$i, $serviceSMS);
         }
     }
 
     /**
      * @return string[]
      */
-    public function getDependencies () {
+    public function getDependencies()
+    {
         return [
             UserFixture::class,
-            CustomerFixture::class
+            CustomerFixture::class,
         ];
     }
 }
