b2f3d3531ddd849f084f3b231aff7275a302c99f
talentdev2020 <polaris.center@outlook.com>
2021-02-02 19:13:47 -0700

    Fix getTheard issues

--- src/Repository/ServiceSMSRepository.php
+++ src/Repository/ServiceSMSRepository.php
@@ -55,24 +55,14 @@ class ServiceSMSRepository extends ServiceEntityRepository
     */
     public function getTrheadsByAdmin(){
         $threadQuery    = "SELECT c.id, c.name,ss.date, ss.message, ss2.unread
-                            FROM (select * from service_sms where date In (select max(date) from service_sms where is_read = 0 group by customer_id)) ss
+                            FROM (select * from service_sms where date In (select max(date) from service_sms group by customer_id)) ss
                             LEFT JOIN customer c ON c.id = ss.customer_id
                             LEFT JOIN (select user_id, customer_id, SUM(CASE WHEN is_read = 0 THEN 1 ELSE 0 END) AS unread from service_sms group by customer_id) ss2 on (ss2.customer_id=ss.customer_id)
-                            order by ss.date DESC";
+                            order by unread =0 ASC , ss.date DESC";
          $statement     = $this->em->getConnection()->prepare($threadQuery);
          $statement->execute();
  
          $result        = $statement->fetchAll();
-         
-         $query         = "select c.id,ss4.date,ss4.message, if(ss1.unread = 1, 0,1) as unreads  from (select customer_id , min(is_read) as unread from service_sms ss group by customer_id)ss1 
-                        LEFT JOIN customer c ON c.id = ss1.customer_id
-                        LEFT JOIN (select customer_id, message, date from service_sms ss2 where date in (select MAX(date) from service_sms ss3 group by customer_id)) ss4 ON ss4.customer_id = ss1.customer_id
-                        where unread = 1 order by date DESC ";
-         $statement     = $this->em->getConnection()->prepare($query);
-         $statement->execute();
-         $readCustomers = $statement->fetchAll();
-
-         $result        = array_merge($result, $readCustomers);
 
          return $result;
     }
@@ -83,21 +73,22 @@ class ServiceSMSRepository extends ServiceEntityRepository
 
         //when there are repairOrders for the customer
         if($isShared){
-            $threadQuery = "SELECT c.id, c.name,ss3.date,ss3.message, count(ss3.id) as unreads from (SELECT  * from (SELECT * from repair_order where primary_advisor_id = ".$userId." ) ss 
-            LEFT JOIN (select id as s_id, primary_customer_id as s_customer_id from repair_order 
-                where id in (select MAX(id) from repair_order group by primary_customer_id)) ss1 on (ss.primary_customer_id = ss1.s_customer_id) ) ss2
-            LEFT JOIN (select * from service_sms where date In (select max(date) from service_sms where is_read = 0 group by customer_id))ss3  on (ss3.customer_id=ss2.primary_customer_id)
-            LEFT JOIN customer c on c.id = ss2.primary_customer_id
-            where ss3.date is not NULL and ss2.s_id in (select id from repair_order where id in (select MAX(id) from repair_order group by primary_customer_id) and primary_advisor_id in (select id from user where share_repair_orders=1))
-            group by c.id";
+            $threadQuery = "SELECT c.id, c.name,ss3.date,ss3.message, ss4.unread from (SELECT  * from (SELECT * from repair_order where primary_advisor_id = ". $userId." group by primary_advisor_id ,primary_customer_id) ss 
+                            LEFT JOIN (select primary_advisor_id as s_advisor_id, primary_customer_id as s_customer_id from repair_order 
+                                where id in (select MAX(id) from repair_order group by primary_customer_id)) ss1 on (ss.primary_customer_id = ss1.s_customer_id) ) ss2
+                            LEFT JOIN (select * from service_sms where date In (select max(date) from service_sms group by customer_id))ss3  on (ss3.customer_id=ss2.primary_customer_id)
+                            LEFT JOIN customer c on c.id = ss2.primary_customer_id
+                            LEFT JOIN (select SUM(CASE WHEN is_read = 0 THEN 1 ELSE 0 END) AS unread, customer_id from service_sms ss3 group by customer_id)ss4 on ss4.customer_id = ss2.primary_customer_id
+                            where ss2.s_advisor_id in (select id from user where share_repair_orders=1)
+                            ";
         }else{
-            $threadQuery = "SELECT c.id, c.name,ss3.date,ss3.message, count(ss3.id) as unreads from (SELECT  * from (SELECT * from repair_order where primary_advisor_id = ".$userId." ) ss 
-                      LEFT JOIN (select id as s_id, primary_customer_id as s_customer_id, primary_advisor_id as s_advisor_id from repair_order 
-                          where id in (select MAX(id) from repair_order group by primary_customer_id)) ss1 on (ss.primary_customer_id = ss1.s_customer_id) ) ss2
-                      LEFT JOIN (select * from service_sms where date In (select max(date) from service_sms where is_read = 0 group by customer_id))ss3  on (ss3.customer_id=ss2.primary_customer_id)
-                      LEFT JOIN customer c on c.id = ss2.primary_customer_id
-                      where ss3.date is not NULL and ss2.s_advisor_id = ".$userId. " 
-                      group by c.id";
+            $threadQuery = "SELECT c.id, c.name,ss3.date,ss3.message, ss4.unread from (SELECT  * from (SELECT * from repair_order where primary_advisor_id = 3 group by primary_advisor_id ,primary_customer_id) ss 
+                        LEFT JOIN (select primary_advisor_id as s_advisor_id, primary_customer_id as s_customer_id from repair_order 
+                            where id in (select MAX(id) from repair_order group by primary_customer_id)) ss1 on (ss.primary_customer_id = ss1.s_customer_id) ) ss2
+                        LEFT JOIN (select * from service_sms where date In (select max(date) from service_sms group by customer_id))ss3  on (ss3.customer_id=ss2.primary_customer_id)
+                        LEFT JOIN customer c on c.id = ss2.primary_customer_id
+                        LEFT JOIN (select SUM(CASE WHEN is_read = 0 THEN 1 ELSE 0 END) AS unread, customer_id from service_sms ss3 group by customer_id)ss4 on ss4.customer_id = ss2.primary_customer_id
+                        where ss2.s_advisor_id = ".$userId;
         }
         
         $statement   = $this->em->getConnection()->prepare($threadQuery);
@@ -126,9 +117,9 @@ class ServiceSMSRepository extends ServiceEntityRepository
                 $statement->execute();
  
                 if($statement->fetch()['user_id'] == $userId){
-                     $query2   = "SELECT count(ss.id) as unread, ss3.message as message, ss3.date as date from service_sms ss 
-                                LEFT JOIN (select date,message,customer_id FROM service_sms ss WHERE date in (SELECT max(date) from service_sms ss2 
-                                   WHERE is_read =0 group by customer_id)) ss3 on ss3.customer_id = ss.customer_id WHERE ss.customer_id = $customerId and ss.is_read =0";
+                     $query2   = "SELECT SUM(CASE WHEN is_read = 0 THEN 1 ELSE 0 END) AS unread, ss3.message as message, ss3.date as date from service_sms ss 
+                                  LEFT JOIN (select date,message,customer_id FROM service_sms ss WHERE date in (SELECT max(date) from service_sms ss2 
+                                     group by customer_id)) ss3 on ss3.customer_id = ss.customer_id WHERE ss.customer_id = 21";
                    
                    $statement  = $this->em->getConnection()->prepare($query2);
                     $statement->execute();
@@ -140,29 +131,25 @@ class ServiceSMSRepository extends ServiceEntityRepository
                             "name"    => $customer['name'],
                             "date"    => $recentSMS['date'],
                             "message" => $recentSMS['message'],
-                            "unreads" => $recentSMS['unread']
+                            "unread" => $recentSMS['unread']
                         ] );
                     }
                 }
             }
         }
         
-        usort($result, function($a, $b){
+        $result1 = array_filter($result, function($item){if($item["unread"]!=="0") return true; });
+        $result2 = array_filter($result, function($item){if($item["unread"]==="0") return true;});
+        
+        usort($result1, function($a, $b){
             return $a["date"] < $b["date"]?1:-1;
         });
-        
-        $query           = "select c.id,ss4.date,ss4.message, if(ss1.unread = 1, 0,1) as unreads  from (select customer_id , min(is_read) as unread from service_sms ss group by customer_id)ss1 
-                        LEFT JOIN customer c ON c.id = ss1.customer_id
-                        LEFT JOIN (select customer_id, message, date from service_sms ss2 where date in (select MAX(date) from service_sms ss3 group by customer_id)) ss4 ON ss4.customer_id = ss1.customer_id
-                        where unread = 1 order by date DESC ";
-        $statement       = $this->em->getConnection()->prepare($query);
-        $statement->execute();
-        $readCustomers   = $statement->fetchAll();
+        usort($result2, function($a, $b){
+            return $a["date"] < $b["date"]?1:-1;
+        });
 
-        $result = array_merge($result, $readCustomers);
-
-        return $result;
+        return array_merge($result1, $result2);
+       
     }
-    
-    
+        
 }
