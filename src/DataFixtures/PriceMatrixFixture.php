2b96d7d63bcc7c7a1b0cc94f94b50690f9e09627
Laramie Rugen <ffgpga08@gmail.com>
2021-02-05 10:22:26 -0500

    Fixed price matrix fixture to be more realistic

--- src/DataFixtures/PriceMatrixFixture.php
+++ src/DataFixtures/PriceMatrixFixture.php
@@ -5,32 +5,23 @@ namespace App\DataFixtures;
 use App\Entity\PriceMatrix;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-use Faker\Factory;
 
-/**
- * Class PriceMatrixFixtures
- *
- * @package App\DataFixtures
- */
-class PriceMatrixFixture extends Fixture {
-    
-    /**
-     * @param ObjectManager $manager
-     */
-    public function load (ObjectManager $manager) {
-        $faker = Factory::create();
- 
-        // Load some PriceMatrixs
-        for ($hours = 0; $hours <= 10; $hours+=0.1) {
+class PriceMatrixFixture extends Fixture
+{
+
+    public function load(ObjectManager $manager)
+    {
+        // Load a Price Matrix
+        for ($hours = 0; $hours <= 10; $hours += 0.1) {
             $priceMatrix = new PriceMatrix();
 
             $priceMatrix->setHours($hours)
-                        ->setPrice($faker->randomFloat(1, 0, 10));
- 
+                        ->setPrice($hours * 110);
+
             $manager->persist($priceMatrix);
             $manager->flush();
 
-            $this->addReference('pricematrix_' . $hours, $priceMatrix);
+            $this->addReference('pricematrix_'.$hours, $priceMatrix);
         }
     }
 }
