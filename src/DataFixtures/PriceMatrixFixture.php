18880afe86faccff3ee364d8de4da58c10f09442
talentdev2020 <polaris.center@outlook.com>
2021-02-05 10:41:18 -0700

    Implement Test

--- src/DataFixtures/PriceMatrixFixture.php
+++ src/DataFixtures/PriceMatrixFixture.php
@@ -12,11 +12,11 @@ class PriceMatrixFixture extends Fixture
     public function load(ObjectManager $manager)
     {
         // Load a Price Matrix
-        for ($hours = 0; $hours <= 10; $hours += 0.1) {
+        for ($hours = 0; $hours <= 1.9; $hours += 0.1) {
             $priceMatrix = new PriceMatrix();
 
             $priceMatrix->setHours($hours)
-                        ->setPrice($hours * 110);
+                        ->setPrice($hours * 5);
 
             $manager->persist($priceMatrix);
             $manager->flush();
