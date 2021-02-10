029dab156a05cec5e81e70c9e233552f44dd86b3
Laramie Rugen <ffgpga08@gmail.com>
2021-02-10 09:26:26 -0500

    Added price matrix to a group so it can be run individually

--- src/DataFixtures/PriceMatrixFixture.php
+++ src/DataFixtures/PriceMatrixFixture.php
@@ -4,10 +4,16 @@ namespace App\DataFixtures;
 
 use App\Entity\PriceMatrix;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
 use Doctrine\Persistence\ObjectManager;
 
-class PriceMatrixFixture extends Fixture
+class PriceMatrixFixture extends Fixture implements FixtureGroupInterface
 {
+
+    public static function getGroups(): array
+    {
+        return ['priceMatrix'];
+    }
 
     public function load(ObjectManager $manager)
     {
