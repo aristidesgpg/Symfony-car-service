c153f9a28f5ffe7b8692958bde073930a301527f
PonyJackal <pony1125@outlook.com>
2021-02-11 08:50:02 -0800

    add phoneValidator in POST repairOrder

--- src/Service/CustomerHelper.php
+++ src/Service/CustomerHelper.php
@@ -116,9 +116,6 @@ class CustomerHelper
                         $msg = 'Invalid phone number';
                         break;
                     }
-                    if (!$this->skipMobileVerification($params) && !$this->phoneValidator->isMobile($v)) {
-                        $msg = 'Phone number is not mobile';
-                    }
                     break;
                 case 'email':
                     if (!empty($v)) {
