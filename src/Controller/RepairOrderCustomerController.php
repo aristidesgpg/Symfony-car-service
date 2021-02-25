259776d49ebf0baacce1b3a3035ebf8876926fc2
Laramie Rugen <ffgpga08@gmail.com>
2021-02-25 16:08:21 -0500

    Review of repair order customers

--- src/Controller/RepairOrderCustomerController.php
+++ src/Controller/RepairOrderCustomerController.php
@@ -4,73 +4,86 @@ namespace App\Controller;
 
 use App\Entity\RepairOrder;
 use App\Entity\RepairOrderCustomer;
-use App\Repository\RepairOrderCustomerRepository;
 use App\Repository\CustomerRepository;
-use App\Response\ValidationResponse;
+use App\Repository\RepairOrderRepository;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Nelmio\ApiDocBundle\Annotation\Model;
 use Swagger\Annotations as SWG;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Doctrine\ORM\EntityManagerInterface;
 
-/**
- * Class RepairOrderCustomerController
- *
- * @package App\Controller
- *
- */
-class RepairOrderCustomerController extends AbstractFOSRestController {
+class RepairOrderCustomerController extends AbstractFOSRestController
+{
 
     /**
-     * @Rest\Post("/api/repair-order-customer/{id}")
+     * @Rest\Post("/api/repair-order-customer")
      *
-     * @SWG\Tag(name="RepairOrderCustomer")
+     * @SWG\Tag(name="Repair Order Customer")
      * @SWG\Post(description="Add a Customer to RepairOrder")
      *
      * @SWG\Parameter(
-     *     name="customer",
+     *     name="repairOrderId",
+     *     type="integer",
+     *     description="Id of a repair order",
+     *     in="formData",
+     *     required=true
+     * )
+     * @SWG\Parameter(
+     *     name="customerId",
      *     type="integer",
      *     description="Id of Customer",
      *     in="formData",
      *     required=true
      * )
-     * 
+     *
      * @SWG\Response(
      *     response="200",
      *     description="Success!",
      *     @SWG\Schema(type="object", ref=@Model(type=RepairOrderCustomer::class, groups=RepairOrderCustomer::GROUPS))
      * )
-     *
      * @SWG\Response(
      *     response="404",
      *     description="Invalid Customer ID"
      * )
      *
-     * @param RepairOrder                $repairOrder
-     * @param Request                    $request
-     * @param CustomerRepository         $customerRepository
-     * @param EntityManagerInterface     $em
-     *
      * @return Response
      */
+    public function new(
+        Request $request,
+        CustomerRepository $customerRepository,
+        RepairOrderRepository $repairOrderRepository,
+        EntityManagerInterface $em
+    ) {
+        $customerId = $request->get('customerId');
+        $repairOrderId = $request->get('repairOrderId');
 
-    public function new (RepairOrder $repairOrder, Request $request, CustomerRepository $customerRepository, 
-                        EntityManagerInterface $em) {
-        $id                  = $request->get('customer');
+        if (!$customerId) {
+            throw new BadRequestHttpException('Missing Required Parameter: customerId');
+        }
 
-        $customer            = $customerRepository->findActive($id);
-        if(!$customer){
-            throw new NotFoundHttpException();
+        if (!$repairOrderId) {
+            throw new BadRequestHttpException('Missing Required Parameter: repairOrderId');
+        }
+
+        $customer = $customerRepository->findActive($customerId);
+        if (!$customer) {
+            throw new NotFoundHttpException('Customer Not Found');
+        }
+
+        $repairOrder = $repairOrderRepository->findByUID($repairOrderId);
+        if (!$repairOrder) {
+            throw new NotFoundHttpException('Repair Order Not Found');
         }
 
         $repairOrderCustomer = new RepairOrderCustomer();
 
         $repairOrderCustomer->setRepairOrder($repairOrder)
                             ->setCustomer($customer);
-        
+
         $em->persist($repairOrderCustomer);
         $em->flush();
 
@@ -83,8 +96,8 @@ class RepairOrderCustomerController extends AbstractFOSRestController {
     /**
      * @Rest\Delete("/api/repair-order-customer/{id}")
      *
-     * @SWG\Tag(name="RepairOrderCustomer")
-     * @SWG\Delete(description="Delete a customer")
+     * @SWG\Tag(name="Repair Order Customer")
+     * @SWG\Delete(description="Delete a secondary customer from a repair order")
      * @SWG\Response(
      *     response=200,
      *     description="success",
@@ -95,17 +108,15 @@ class RepairOrderCustomerController extends AbstractFOSRestController {
      *         )
      * )
      *
-     * @param RepairOrderCustomer                 $repairOrderCustomer
-     * @param EntityManagerInterface $em
-     *
      * @return Response
      */
-    public function delete (RepairOrderCustomer $repairOrderCustomer, EntityManagerInterface $em) {
- 
+    public function delete(RepairOrderCustomer $repairOrderCustomer, EntityManagerInterface $em)
+    {
+
         $em->remove($repairOrderCustomer);
         $em->flush();
 
-        return $this->handleView($this->view('RepairOrderCustomer Deleted', Response::HTTP_OK));
+        return $this->handleView($this->view('Customer Removed from Repair Order', Response::HTTP_OK));
     }
 
 }
