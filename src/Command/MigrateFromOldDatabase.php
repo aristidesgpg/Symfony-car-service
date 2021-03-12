e58497ff317df8cb2c6c06d62f7b45bd284078e5
talentdev2020 <polaris.center@outlook.com>
2021-03-12 08:18:58 -0700

    Implement customer and repairOrder migration

--- src/Command/MigrateFromOldDatabase.php
+++ src/Command/MigrateFromOldDatabase.php
@@ -4,6 +4,10 @@ namespace App\Command;
 
 use Doctrine\ORM\EntityManager;
 use App\Entity\User;
+use App\Entity\RepairOrder;
+use App\Entity\Customer;
+use App\Entity\RepairOrderVideo;
+use App\Entity\RepairOrderVideoInteraction;
 use Exception;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -43,6 +47,9 @@ class MigrateFromOldDatabase extends Command
 
     protected $oldAdminIds;
     protected $oldAdvisorIds;
+    protected $oldTechnicanIds;
+    protected $oldRepairOrderIds;
+    protected $oldCustomerIds;
     /**
      * constructor.
      */
@@ -51,7 +58,11 @@ class MigrateFromOldDatabase extends Command
         $this->passwordEncoder = $passwordEncoder;
         $this->connection = $ri->getConnection("iservice2");
         $this->em = $em;
+        $this->oldAdminIds = array();
         $this->oldAdvisorIds = array();
+        $this->oldTechnicanIds = array();
+        $this->oldRepairOrderIds = array();
+        $this->oldCustomerIds = array();
         parent::__construct();
     } 
 
@@ -72,9 +83,15 @@ class MigrateFromOldDatabase extends Command
         
         // $statement->bindValue('emp', );
         $this->admin();
+        $output->writeln("Admin done");
         $this->advisor();
+        $output->writeln("Advisor done");
+        $this->customer();
+        $output->writeln("Customer done");
+        $this->technican();
+        $output->writeln("Technican done");
         
-        $output->writeln(json_encode($this->oldAdminIds));
+        $output->writeln(json_encode($this->oldCustomerIds));
         return "success";
     }
 
@@ -119,6 +136,47 @@ class MigrateFromOldDatabase extends Command
             }
         }
     }
+    private function technican(){
+        $statement = $this->connection->prepare(
+            'SELECT * FROM technican'
+        );
+        
+        $statement->execute();
+        $rows = $statement->fetchAll();
+
+        $userRepo = $this->em->getRepository(User::class);
+        
+        foreach($rows as $row){
+            $oldUser = $userRepo->findOneBy(['first_name' => $row['first_name', 'last_name' => $row['last_name' ] ] );
+            array_push($this->oldTechnicanIds, $row['id']);
+            $user = new User();
+            if($oldUser){
+                $this->oldTechnicanIds[ $row['id'] ] = $oldUser->getId();
+            } else {
+                if($row['password']){
+                    $password = $row['password'];
+                } else {
+                    $password = $this->passwordEncoder->encodePassword($user, 'test');
+                }
+               
+                $user->setFirstName( $row['first_name'] )
+                     ->setLastName( $row['last_name'] )
+                     ->setEmail( $row['email'] )
+                     ->setCertification( $row['certification'] )
+                     ->setExperience( $row['experience'] )
+                     ->setActive( $row['active'] )
+                     ->setPassword( $password )
+                     ->setSecurityAnswer( $row['security_answer'] )
+                     ->setSecurityQuestion( 'security_question' )
+                     ->setRole( 'ROLE_TECHNICIAN' );
+                
+                $this->em->persist( $user );
+                $this->em->flush();
+                
+                $this->oldTechnicanIds[ $row['id'] ] = $user->getId();
+            }
+        }
+    }
     private function advisor(){
         $statement = $this->connection->prepare(
             'SELECT * FROM advisor'
@@ -148,7 +206,6 @@ class MigrateFromOldDatabase extends Command
                      ->setSecurityAnswer( $row['security_answer'] )
                      ->setProcessRefund( $row['can_give_refund'])
                      ->setShareRepairOrders( $row['express_advisor'])
-                     ->setPreviewDeviceTokens( $row['preview_tokens'] )
                      ->setRole( 'ROLE_SERVICE_ADVISOR' );
                 
                 $this->em->persist( $user );
@@ -160,16 +217,16 @@ class MigrateFromOldDatabase extends Command
     }
     private function CAQLog(){
         $statement = $this->connection->prepare(
-            'SELECT * FROM advisor'
+            'SELECT * FROM c_a_q_log'
         );
         
         $statement->execute();
-        $rows = $statement->fetchAll();
+        $rows = $statement->fetchAllAssociative();
 
-        $repo = $this->em->getRepository(User::class);
+        $userRepo = $this->em->getRepository(User::class);
         
         foreach($rows as $row){
-            $oldUser = $repo->findOneBy(['email' => $row['email']]);
+            $oldUser = $userRepo->findOneBy(['email' => $row['email']]);
             array_push($this->oldAdvisorIds, $row['id']);
 
             if($oldUser){
@@ -194,6 +251,125 @@ class MigrateFromOldDatabase extends Command
                 $this->em->flush();
                 
                 $this->oldAdvisorIds[ $row['id'] ] = $user->getId();
+            }
+        }
+    }
+
+    private function repairOrder(){
+        $statement = $this->connection->prepare(
+            'SELECT * FROM repair_order'
+        );
+        
+        $statement->execute();
+        $rows = $statement->fetchAllAssociative();
+        $userRepo = $this->em->getRepository(User::class);
+        $customerRepo = $this->em->getRepository(Customer::class);
+        $repairOrderRepo = $this->em->getRepository(RepairOrder::class);
+
+        foreach($rows as $row){
+            array_push($this->oldRepairOrderIds, $row['id']);
+
+            $oldRepairOrder = $repairOrderRepo->findOneBy(['number' => $row['number']]);
+            if($oldRepairOrder) {
+                $this->oldAdvisorIds[ $row['id'] ] = $oldRepairOrder->getId();
+            } else {
+                $customerId = $this->oldCustomerIds[ $row['primary_customer_id'] ];
+                $customer = $customerRepo->findOneBy(['id' => $customerId]);
+    
+                $technicanId = $this->oldTechnicanIds[ $row['technican_id' ] ];
+                $technican = $userRepo->findOneBy(['id' => $technicanId] );
+                
+                $advisorId = $this->oldAdvisorIds[ $row['advisor_id' ] ];
+                $advisor = $userRepo->findOneBy(['id' => $advisorId] );
+    
+                $repairOrder = new RepairOrder();
+                $repairOrder->setPrimaryCustomer( $customer )
+                            ->setPrimaryTechnician($technican)
+                            ->setPrimaryAdvisor($advisor)
+                            ->setNumber($row['number'])
+                            ->setStartValue($row['value'])
+                            ->setFinalValue($row['finalValue'])
+                            ->setApprovedValue($row['approvedValue'])
+                            ->setDateClosed($row['closed_date'])
+                            ->setDateCreated($row['date'])
+                            ->setWaiter($row['waiter'])
+                            ->setPickupDate($row['pickup_date')
+                            ->setLinkHash($row['link_hash'])
+                            ->setYear($row['year'])
+                            ->setMake($row['make'])
+                            ->setModel($row['model'])
+                            ->setVin($row['vin'])
+                            ->setDeleted($row['inactive'])
+                            ->setMiles($row['miles'])
+                            ->setWaiverSignature($row['waiver'])
+                            ->setWaiverVerbiage($row['waiverVerbiage'])
+                            ->setUpgradeQue($row['upgradeQueue']);
+                
+                $this->em->persist( $repairOrder );
+                $this->em->flush();
+                
+                if($row['video']) {
+                    $repairOrderVideo  = new RepairOrderVideo();
+                    $repairOrderVideoInteraction  = new RepairOrderVideoInteraction();
+                    $repairOrderVideo->setRepairOrder($repairOrder)
+                                     ->setTechnician($technican)
+                                     ->setPath($row['video'])
+                                     ->setStatus("Uploaded")
+                                     ->setDateUploaded($row['date']);
+                    
+                    $this->em->persist( $repairOrder );
+                    $this->em->flush();
+    
+                    $repairOrderVideoInteraction->setRepairOrderVideo($repairOrderVideo)
+                                                ->setUser($technican)
+                                                ->setCustomer($customer)
+                                                ->setType("Uploaded")
+                                                ->setDate($row['date']);
+                    $this->em->persist( $repairOrder );
+                    $this->em->flush();
+                }
+                $this->oldAdvisorIds[ $row['id'] ] = $repairOrder->getId();
+            }
+        }
+    }
+    private function customer(){
+        $statement = $this->connection->prepare(
+            'SELECT * FROM customer'
+        );
+        $userRepo = $this->em->getRepository(User::class);
+        $customerRepo = $this->em->getRepository(Customer::class);
+        $statement->execute();
+        $rows = $statement->fetchAll();
+
+        foreach($rows as $row){
+            $phone = $row['phone'];
+            $cleanPhone = preg_replace('/[^0-9]/', '', $phone);
+            if(strlen($cleanPhone) > 10){
+                $cleanPhone = substr($cleanPhone, 1, 10);
+            }
+
+            $oldCustomer = $customerRepo->findOneBy(['phone' => $cleanPhone]);
+            array_push($this->oldCustomerIds, $row['id']);
+            
+            if($oldCustomer){
+                $this->oldCustomerIds[ $row['id'] ] = $oldCustomer->getId();
+            } else {
+                $customer = new Customer();
+                
+                $customer->setMobileConfirmed($row['phone_validated'])
+                         ->setName($row['name'] ? $row['name'] : "No Name")
+                         ->setEmail($row['email'])
+                         ->setPhone($cleanPhone);
+
+                if( $row['added_by_id'] ) {
+                    $user = $userRepo->findOneBy(['id' => $this->oldAdvisorIds[ $row['added_by_id'] ] ]);
+                    $customer->setAddedBy( $user );
+                }
+                
+                $this->em->persist( $customer );
+                $this->em->flush();
+                
+                $this->oldCustomerIds[ $row['id'] ] = $customer->getId();
             }
         }
     }
