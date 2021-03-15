c9a0fa0996ec3d2e670cadc6046b87fd01618a16
PonyJackal <pony1125@outlook.com>
2021-03-15 13:37:37 -0400

    send sms with repairOrderURL

--- src/Controller/RepairOrderQuoteController.php
+++ src/Controller/RepairOrderQuoteController.php
@@ -8,6 +8,8 @@ use App\Helper\iServiceLoggerTrait;
 use App\Repository\RepairOrderQuoteRepository;
 use App\Repository\RepairOrderRepository;
 use App\Service\RepairOrderQuoteHelper;
+use App\Service\SettingsHelper;
+use App\Service\TwilioHelper;
 use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use Exception;
@@ -15,6 +17,7 @@ use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Nelmio\ApiDocBundle\Annotation\Model;
 use Swagger\Annotations as SWG;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
@@ -288,7 +291,10 @@ class RepairOrderQuoteController extends AbstractFOSRestController
     public function customerSend(
         Request $request,
         RepairOrderQuoteRepository $repairOrderQuoteRepository,
-        EntityManagerInterface $em
+        EntityManagerInterface $em,
+        TwilioHelper $twilioHelper,
+        SettingsHelper $settingsHelper,
+        ParameterBagInterface $parameterBag
     ): Response {
         $repairOrderQuoteID = $request->get('repairOrderQuoteID');
         $status = 'Sent';
@@ -315,6 +321,10 @@ class RepairOrderQuoteController extends AbstractFOSRestController
         // Update repairOrder quote_status
         $repairOrder->setQuoteStatus($status);
         // send repair order link to the customer
+        $serviceTextQuote = $settingsHelper->getSetting('serviceTextQuote');
+        $customerURL = $parameterBag->get('customer_url');
+        $repairOrderURL = $serviceTextQuote.$customerURL.$repairOrder->getLinkHash();
+        $twilioHelper->sendSms($repairOrder->getPrimaryCustomer(), $repairOrderURL);
 
         $em->persist($repairOrder);
         $em->persist($repairOrderQuote);
