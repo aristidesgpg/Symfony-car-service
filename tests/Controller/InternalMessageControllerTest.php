6dc4947bdc1c6a2b4d9bc4eaa29a14b1c659bbe9
gokhanmelma <gokhanmelma@gmail.com>
2021-03-16 15:46:10 -0400

    Fix contflicts

--- tests/Controller/InternalMessageControllerTest.php
+++ tests/Controller/InternalMessageControllerTest.php
@@ -7,17 +7,19 @@ use App\Service\Authentication;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Response;
 
-class InternalMessageControllerTest extends WebTestCase {
+class InternalMessageControllerTest extends WebTestCase
+{
     private $client = null;
 
     private $token;
-    
+
     private $authenticatedUserId;
 
     /**
      * {@inheritDoc}
      */
-    public function setUp() {
+    public function setUp()
+    {
         $this->client = static::createClient();
 
         $user = self::$container->get('doctrine')
@@ -29,33 +31,37 @@ class InternalMessageControllerTest extends WebTestCase {
                                 ->getOneOrNullResult();
 
         $authentication = self::$container->get(Authentication::class);
-        $ttl            = 31536000;
-        $this->token    = $authentication->getJWT($user->getEmail(), $ttl);
+        $ttl = 31536000;
+        $this->token = $authentication->getJWT($user->getEmail(), $ttl);
         $this->authenticatedUserId = $user->getId();
     }
 
-    public function testGetThreads() {
+    public function testGetThreads()
+    {
         if (!$this->token) {
             $this->assertEmpty($this->token, 'Token is null');
+
             return;
         }
-        
+
         // Page Not Found
         $page = 0;
-        $this->requestAction('GET', '/threads?page=' . $page );
+        $this->requestAction('GET', '/threads?page='.$page);
         $this->assertEquals(Response::HTTP_BAD_REQUEST, $this->client->getResponse()->getStatusCode());
 
         // Ok
         $page = 1;
-        $this->requestAction('GET', '/threads?page=' . $page );
+        $this->requestAction('GET', '/threads?page='.$page);
         $threadsData = json_decode($this->client->getResponse()->getContent());
         $this->assertResponseIsSuccessful();
         $this->assertGreaterThanOrEqual(0, $threadsData->totalResults);
     }
 
-    public function testGetMessagesNewest() {
+    public function testGetMessagesNewest()
+    {
         if (!$this->token) {
             $this->assertEmpty($this->token, 'Token is null');
+
             return;
         }
 
@@ -70,28 +76,31 @@ class InternalMessageControllerTest extends WebTestCase {
         if ($user) {
             // Page Not Found
             $otherUserId = $user->getId();
-            $page        = 0;
+            $page = 0;
 
-            $this->requestAction('GET', '/messages?otherUserId=' . $otherUserId . '&page=' . $page);
+            $this->requestAction('GET', '/messages?otherUserId='.$otherUserId.'&page='.$page);
             $this->assertEquals(Response::HTTP_NOT_FOUND, $this->client->getResponse()->getStatusCode());
 
             // Ok
             $otherUserId = $user->getId();
-            $page        = 1;
+            $page = 1;
 
-            $this->requestAction('GET', '/messages?otherUserId=' . $otherUserId . '&page=' . $page);
+            $this->requestAction('GET', '/messages?otherUserId='.$otherUserId.'&page='.$page);
             $messagesData = json_decode($this->client->getResponse()->getContent());
             $this->assertResponseIsSuccessful();
             $this->assertGreaterThanOrEqual(0, $messagesData->totalResults);
         } else {
             $this->assertEmpty($user, 'User is null');
+
             return;
         }
     }
 
-    public function testSendMessage() {
+    public function testSendMessage()
+    {
         if (!$this->token) {
             $this->assertEmpty($this->token, 'Token is null');
+
             return;
         }
 
@@ -105,19 +114,19 @@ class InternalMessageControllerTest extends WebTestCase {
         $user = $users[1];
         if ($user) {
             // Ok
-            $toId    = $user->getId();
-            $message = "Hello World";
+            $toId = $user->getId();
+            $message = 'Hello World';
 
-            $this->requestAction('POST', '?toId=' . $toId . '&message=' . $message);
+            $this->requestAction('POST', '?toId='.$toId.'&message='.$message);
             $messagesData = json_decode($this->client->getResponse()->getContent());
             $this->assertResponseIsSuccessful();
             $this->assertEquals($message, $messagesData->message);
 
             // Message from You to You
-            $toId    = $this->authenticatedUserId;
-            $message = "Hello World";
+            $toId = $this->authenticatedUserId;
+            $message = 'Hello World';
 
-            $this->requestAction('POST', '?toId=' . $toId . '&message=' . $message);
+            $this->requestAction('POST', '?toId='.$toId.'&message='.$message);
             $this->assertEquals(Response::HTTP_BAD_REQUEST, $this->client->getResponse()->getStatusCode());
 
             // Parameter(s) Required
@@ -125,24 +134,27 @@ class InternalMessageControllerTest extends WebTestCase {
             $this->assertEquals(Response::HTTP_BAD_REQUEST, $this->client->getResponse()->getStatusCode());
         } else {
             $this->assertEmpty($user, 'User is null');
+
             return;
         }
     }
 
-    private function requestAction($method, $apiUrl, $params=[]) {
+    private function requestAction($method, $apiUrl, $params = [])
+    {
         $crawler = $this->client->request($method, '/api/internal-message'.$apiUrl, $params, [], [
             'HTTP_Authorization' => 'Bearer '.$this->token,
-            'HTTP_CONTENT_TYPE'  => 'application/json',
-            'HTTP_ACCEPT'        => 'application/json',
+            'HTTP_CONTENT_TYPE' => 'application/json',
+            'HTTP_ACCEPT' => 'application/json',
         ]);
     }
 
     /**
      * {@inheritDoc}
      */
-    protected function tearDown() {
+    protected function tearDown()
+    {
         parent::tearDown();
         $this->client = null;
-        $this->token  = null;
+        $this->token = null;
     }
 }
