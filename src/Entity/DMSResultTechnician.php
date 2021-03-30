53df504e0b1097a3e9a7d3fb55a1d0179f881dd8
Brian Hurst <brian-hurst@outlook.com>
2021-03-30 15:36:05 -0600

    DMS Updates

---
+++ src/Entity/DMSResultTechnician.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Entity;
+
+class DMSResultTechnician
+{
+    /**
+     * @var ?int
+     */
+    private $id;
+
+    /**
+     * @var ?string
+     */
+    private $firstName;
+
+    /**
+     * @var ?string
+     */
+    private $lastName;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param $id
+     */
+    public function setId($id): DMSResultTechnician
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @param $firstName
+     */
+    public function setFirstName($firstName): DMSResultTechnician
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @param mixed $lastName
+     */
+    public function setLastName($lastName): DMSResultTechnician
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+}
