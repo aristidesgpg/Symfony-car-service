fbe210d53b33174926f9ff9436b8029ac867ebe7
PonyJackal <pony1125@outlook.com>
2021-04-08 05:25:06 -0700

    update query to prevent duplication

--- src/Service/InternalMessageHelper.php
+++ src/Service/InternalMessageHelper.php
@@ -71,7 +71,7 @@ class InternalMessageHelper
                     ) jj
                     GROUP BY case when jj.to_id = {$userId} then jj.from_id when jj.from_id = {$userId} then jj.to_id END
                 ) im
-                ON i.date = im.MaxDate
+                ON i.id = im.id
                 INNER JOIN user u
                 ON u.id = case when i.to_id = {$userId} then i.from_id when i.from_id = {$userId} then i.to_id END AND CONCAT(u.first_name,' ', u.last_name) LIKE '%{$searchTerm}%'
                 LEFT JOIN 
