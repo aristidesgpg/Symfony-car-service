f77b7465204ab4cedb431ded1512e635f37af588
Brian Hurst <brian-hurst@outlook.com>
2021-04-19 08:15:04 -0600

    Temporary stub code for opcodes.

--- src/Service/DMS/AutoMateClient.php
+++ src/Service/DMS/AutoMateClient.php
@@ -408,6 +408,7 @@ class AutoMateClient extends AbstractDMSClient
     public function getOperationCodes(): array
     {
         // TODO: Implement getOperationCodes() method.
+        throw new AccessDeniedException('Not Implemented for this DMS.');
     }
 
     public function getParts(): array
