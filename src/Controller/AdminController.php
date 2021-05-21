62af09af2e4a4ccd63f0ebc5a61fa1830c726ca3
talentdev2020 <polaris.center@outlook.com>
2021-05-21 10:47:04 -0700

    create settings function in admincontroller

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace App\Controller;
+
+use App\Response\ValidationResponse;
+use App\Service\SettingsHelper;
+use Doctrine\ORM\EntityManagerInterface;
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use Swagger\Annotations as SWG;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\Security\Core\Security;
+
+class AdminController extends AbstractFOSRestController
+{
+    /**
+     * @Rest\Get("/api/get-settings")
+     *
+     * @SWG\Tag(name="Admin")
+     * @SWG\Get(description="Get settings")
+     *
+     * @SWG\Response(
+     *     response="200",
+     *     description="Success!",
+     *     @SWG\Schema(
+     *         type="object",
+     *         @SWG\Property(property="hasPayments", type="boolean"),
+     *         @SWG\Property(property="paymentToken", type="string"),
+     *         @SWG\Property(property="myReviewActivated", type="boolean"),
+     *         @SWG\Property(property="usingAutomate", type="boolean"),
+     *         @SWG\Property(property="usingCdk", type="boolean"),
+     *         @SWG\Property(property="usingDealerBuilt", type="boolean"),
+     *         @SWG\Property(property="usingDealerTrack", type="boolean"),
+     *     )
+     * )
+     * 
+     * @SWG\Response(
+     *     response="403",
+     *     description="Invalid permission"
+     * )
+     *
+     */
+    public function getSettings( SettingsHelper $settingsHelper, Security $security ) {
+        // if (!$security->isGranted('ROLE_ADMIN') ||  !$this->getUser()->getExternalAuthentication()) {
+        //     throw new BadRequestHttpException('The user should be admin and the external_authenticaion feature should be available');
+        // }
+
+        $json = [
+            'hasPayments' => $settingsHelper->getSetting("hasPayments"),
+            'paymentToken' => $settingsHelper->getSetting("paymentToken"),
+            'myReviewActivated' => $settingsHelper->getSetting("myReviewActivated"),
+            'usingAutomate' =>$settingsHelper->getSetting("usingAutomate"),
+            'usingCdk' => $settingsHelper->getSetting("usingCdk"),
+            'usingDealerBuilt' => $settingsHelper->getSetting("usingDealerBuilt"),
+            'usingDealerTrack' => $settingsHelper->getSetting("usingDealerTrack"),
+         ];
+
+        $view = $this->view($json);
+        return $this->handleView($view);
+    }
+
+    /**
+     * @Rest\Post("/api/admin/set-settings")
+     *
+     * @SWG\Tag(name="Admin")
+     * @SWG\Post(description="Create settings")
+     *
+     * @SWG\Parameter(
+     *     name="hasPayments",
+     *     type="boolean",
+     *     description="Set the hasPayments setting",
+     *     in="formData",
+     * )
+     * 
+     * @SWG\Parameter(
+     *     name="paymentToken",
+     *     type="string",
+     *     description="Set the paymentToken setting",
+     *     in="formData",
+     * )
+     * 
+     * @SWG\Parameter(
+     *     name="myReviewActivated",
+     *     type="boolean",
+     *     description="Set the myReviewActivated setting",
+     *     in="formData",
+     * )
+     * 
+     * @SWG\Parameter(
+     *     name="usingAutomate",
+     *     type="boolean",
+     *     description="Set the usingAutomate setting",
+     *     in="formData",
+     * )
+     * 
+     * @SWG\Parameter(
+     *     name="usingCdk",
+     *     type="boolean",
+     *     description="Set the usingCdk setting",
+     *     in="formData",
+     * )
+     * 
+     * @SWG\Parameter(
+     *     name="usingDealerBuilt",
+     *     type="boolean",
+     *     description="Set the usingDealerBuilt setting",
+     *     in="formData",
+     * )
+     * 
+     * @SWG\Parameter(
+     *     name="usingDealerTrack",
+     *     type="boolean",
+     *     description="Set the usingDealerTrack setting",
+     *     in="formData",
+     * )
+     * 
+     * @SWG\Response(
+     *     response="200",
+     *     description="Success!",
+     * )
+     *
+     * @SWG\Response(
+     *     response="403",
+     *     description="Invalid permission"
+     * )
+     *
+     * @return Response
+     */
+    public function new(Request $request, SettingsHelper $settingsHelper, Security $security)
+    {
+        if (!$security->isGranted('ROLE_ADMIN') &&  !$this->getUser()->getExternalAuthentication()) {
+            throw new BadRequestHttpException('The user should be admin and the external_authenticaion feature should be available');
+        }
+        
+        if (count($request->request->all()) !== 0) {
+            $settingsHelper->commitSettings($request->request->all());
+        }
+
+        $view = $this->view("Successfully saved", Response::HTTP_OK);
+
+        return $this->handleView($view);
+    }
+}
